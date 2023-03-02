80bf71edbdc20342e71cf3cc5da7d20e4caf71a4
d10n <d10n@redhat.com>
2023-03-02 12:00:50 -0500

    Add ~bootstrap alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from 'node:path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
 })
