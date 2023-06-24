fcc6bad3b38f7ed42fb2b60207fed42d3350a088
d10n <d10n@redhat.com>
2023-06-23 23:16:13 -0400

    Rename OSIM_BACKEND to OSIM_BACKENDS

--- build/20-set-backend.sh
+++ build/20-set-backend.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ "x$OSIM_BACKEND" = x ]; then
-    printf '%s\n' 'OSIM_BACKEND not in environment. Setting default backend.json contents.'
-    OSIM_BACKEND='{"osidb":"http://osidb-service:8000"}'
+if [ "x$OSIM_BACKENDS" = x ]; then
+    printf '%s\n' 'OSIM_BACKENDS not in environment. Setting default backend.json contents.'
+    OSIM_BACKENDS='{"osidb":"http://osidb-service:8000"}'
 fi
 
 # Store to /dev/shm because /var/www/nginx/html is read-only and /tmp might get messy
-printf '%s\n%s\n' 'Writing /dev/shm/backend.json:' "$OSIM_BACKEND"
-printf '%s\n' "$OSIM_BACKEND" >/dev/shm/backend.json
+printf '%s\n%s\n' 'Writing /dev/shm/backend.json:' "$OSIM_BACKENDS"
+printf '%s\n' "$OSIM_BACKENDS" >/dev/shm/backend.json
 chmod 444 /dev/shm/backend.json
