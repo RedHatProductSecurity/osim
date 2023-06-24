4841908fd6de7da3726699984c966c19361ad1ee
d10n <d10n@redhat.com>
2023-06-23 22:40:04 -0400

    Actually, use backend.json in tmpfs
    nginx doesn't read arbitrary environment variables

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,6 @@ RUN yarn build
 # Production stage
 FROM nginxinc/nginx-unprivileged:latest
 COPY ./build/nginx-osim.conf /etc/nginx/conf.d/default.conf
-#COPY ./build/20-set-backend.sh /docker-entrypoint.d/
-COPY ./build/20-set-backend-env.shenv /docker-entrypoint.d/
+COPY ./build/20-set-backend.sh /docker-entrypoint.d/
 # Copy the built files to the default Nginx directory
 COPY --from=dev /app/dist /usr/share/nginx/html
--- build/20-set-backend.sh
+++ build/20-set-backend.sh
@@ -4,5 +4,8 @@ if [ "x$OSIM_BACKEND" = x ]; then
     printf '%s\n' 'OSIM_BACKEND not in environment. Setting default backend.json contents.'
     OSIM_BACKEND='{"osidb":"http://osidb-service:8000"}'
 fi
-printf '%s\n%s\n' 'Writing /usr/share/nginx/html/backend.json:' "$OSIM_BACKEND"
-printf '%s\n' "$OSIM_BACKEND" >/usr/share/nginx/html/backend.json
+
+# Store to /dev/shm because /var/www/nginx/html is read-only and /tmp might get messy
+printf '%s\n%s\n' 'Writing /dev/shm/backend.json:' "$OSIM_BACKEND"
+printf '%s\n' "$OSIM_BACKEND" >/dev/shm/backend.json
+chmod 444 /dev/shm/backend.json
