bdf1befec00c87f0eb22e34ebb68fd0ed4a08811
d10n <d10n@redhat.com>
2023-07-03 20:56:23 -0400

    Use OsidbAuthService in FlawService

--- src/services/FlawService.ts
+++ src/services/FlawService.ts
@@ -1,16 +1,63 @@
 import axios from 'axios';
+import {osidbFetch} from '@/services/OsidbAuthService';
 
 export async function getFlaws() {
   // TODO add filtering parameters
   // axios.get('http://127.0.0.1:4010/osidb/api/v1/flaws?bz_id=999.1777106091507&changed_after=2016-05-25T04%3A00%3A00.0Z&changed_before=1953-04-15T05%3A00%3A00.0Z&created_dt=1997-02-22T05%3A00%3A00.0Z&cve_id=suscipit,quia,dignissimos&cvss2=nobis&cvss2_score=-3.12820402011057e%2B38&cvss3=nam&cvss3_score=2.2240193839647933e%2B38&cwe_id=reprehenderit&description=sed&embargoed=false&exclude_fields=pariatur&flaw_meta_type=enim,sed,enim&impact=LOW&include_fields=et,quisquam,sunt,aut&include_meta_attr=ullam,libero,at,alias&reported_dt=1972-11-30T00%3A00%3A00.0Z&resolution=DUPLICATE&search=unde&source=PHP&state=NEW&statement=sunt&summary=maiores&title=reprehenderit&tracker_ids=eum,cum,at,odio,a&type=WEAKNESS&unembargo_dt=1949-12-22T00%3A00%3A00.0Z&updated_dt=1973-03-16T05%3A00%3A00.0Z&uuid=c605cdc8-0f63-c5ec-d32d-75c184147eba')
   // axios.get('https://osidb-stage.example.com/osidb/api/v1/flaws')
-  return axios.get('/mock/staging-flaws.json')
+  // return axios.get('/mock/prod-flaws.json')
+  // return axios.get('/mock/prod-flaws.json')
+  if (import.meta.env.VITE_RUNTIME_LEVEL === 'MOCK') {
+    return axios.get('/mock/new-flaws-stage.json')
+  }
+  if (import.meta.env.VITE_RUNTIME_LEVEL === 'DEV') {
+    // return osidbFetch({
+    //   method: 'get',
+    //   url: '/osidb/api/v1/flaws',
+    // });
+    return osidbFetch({
+      method: 'get',
+      url: '/osidb/api/v1/flaws',
+      params: {
+        include_fields: 'cve_id,uuid,impact,source,created_dt,updated_dt,classification,is_major_incident,title,state,unembargo_dt',
+      },
+    });
+  }
+  return osidbFetch({
+    method: 'get',
+    url: '/osidb/api/v1/flaws',
+    params: {
+      include_fields: 'cve_id,uuid,impact,source,created_dt,updated_dt,classification,is_major_incident,title,state,unembargo_dt',
+    },
+  });
+  // if (import.meta.env.VITE_RUNTIME_LEVEL === 'PROD') {
+  //
+  // }
+}
+
+/**
+ * Get the flaws for the queue, filtered to required fields.
+ */
+export async function getFlawQueue() {
+  // TODO add filtering parameters
+  // axios.get('http://127.0.0.1:4010/osidb/api/v1/flaws?bz_id=999.1777106091507&changed_after=2016-05-25T04%3A00%3A00.0Z&changed_before=1953-04-15T05%3A00%3A00.0Z&created_dt=1997-02-22T05%3A00%3A00.0Z&cve_id=suscipit,quia,dignissimos&cvss2=nobis&cvss2_score=-3.12820402011057e%2B38&cvss3=nam&cvss3_score=2.2240193839647933e%2B38&cwe_id=reprehenderit&description=sed&embargoed=false&exclude_fields=pariatur&flaw_meta_type=enim,sed,enim&impact=LOW&include_fields=et,quisquam,sunt,aut&include_meta_attr=ullam,libero,at,alias&reported_dt=1972-11-30T00%3A00%3A00.0Z&resolution=DUPLICATE&search=unde&source=PHP&state=NEW&statement=sunt&summary=maiores&title=reprehenderit&tracker_ids=eum,cum,at,odio,a&type=WEAKNESS&unembargo_dt=1949-12-22T00%3A00%3A00.0Z&updated_dt=1973-03-16T05%3A00%3A00.0Z&uuid=c605cdc8-0f63-c5ec-d32d-75c184147eba')
+  // axios.get('https://osidb-stage.example.com/osidb/api/v1/flaws')
+  return axios.get('/mock/prod-flaws.json')
 }
 
 export async function getFlaw(uuid: string) {
-  return axios.get('/mock/staging-flaws.json')
-      .then(response => {
-        const flaw = response.data.results.find((flaw: { uuid: string; }) => flaw.uuid === uuid);
-        return flaw;
-      })
+  if (import.meta.env.VITE_RUNTIME_LEVEL === 'MOCK') {
+    return axios.get('/mock/new-flaws-stage.json')
+        .then(response => {
+          const flaw = response.data.results.find((flaw: { uuid: string; }) => flaw.uuid === uuid);
+          return flaw;
+        })
+  }
+
+  return osidbFetch({
+    method: 'get',
+    url: `/osidb/api/v1/flaws/${uuid}`
+  }).then(response => {
+    return response.data;
+  })
 }
