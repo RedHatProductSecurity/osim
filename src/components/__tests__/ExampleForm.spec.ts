cbe924f532750d2dfae93c1e9d62458308f7d76e
d10n <d10n@redhat.com>
2023-10-03 11:29:11 -0400

    Add vitest

---
+++ src/components/__tests__/ExampleForm.spec.ts
@@ -0,0 +1,22 @@
+import {describe, it, expect} from 'vitest';
+
+import {mount} from '@vue/test-utils';
+import ExampleForm from '../ExampleForm.vue';
+import {IMaskDirective} from 'vue-imask';
+import {type Directive} from 'vue';
+
+describe('ExampleForm', () => {
+  it('renders properly', () => {
+    const wrapper = mount(ExampleForm, {
+      props: {
+        msg: 'Example Form',
+      },
+      global: {
+        directives: {
+          imask: IMaskDirective as Directive,
+        },
+      },
+    });
+    expect(wrapper.text()).toContain('Example Form');
+  })
+});
