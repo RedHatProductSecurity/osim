d77f18ad3762f24300081789b37a1075e2d1b40d
d10n <d10n@redhat.com>
2023-11-15 07:58:07 -0500

    Add LabelDiv

---
+++ src/components/widgets/LabelDiv.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {nextTick, onMounted, ref} from 'vue';
+
+const props = defineProps<{
+  modelValue: string,
+  label: string,
+  error?: string,
+}>();
+
+</script>
+
+<template>
+  <div class="osim-input mb-3 border-start ps-3">
+    <span class="form-label">
+      {{ label }}
+      <!--attrs: {{ $attrs }}-->
+    </span>
+    <slot></slot>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
