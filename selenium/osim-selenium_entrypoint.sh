ba73f795a8da024a101411f8c3e7b82999500172
d10n <d10n@redhat.com>
2023-12-19 10:26:37 -0500

    Extract entrypoint.d for selenium image

--- selenium/osim-selenium_entrypoint.sh
+++ selenium/osim-selenium_entrypoint.sh
@@ -3,15 +3,9 @@
 set -x
 set -e
 
-cleanup() { rm -f /tmp/krb5.keytab; }
-trap cleanup EXIT
-sudo install -o"$(id -u)" -g"$(id -g)" -m400 /keytabs/krb5.keytab /tmp/krb5.keytab
-
-principal="$( klist -kt /tmp/krb5.keytab | grep -Eo -m1 '\w+@[A-Z.]+' )"
-kinit -k -t /tmp/krb5.keytab "$principal"
-klist
-
-cleanup
+for exe in /entrypoint.d/*; do
+    if [[ -x "$exe" ]]; then "$exe"; fi
+done
 
 # from selenium image:
 # CMD ["/opt/bin/entry_point.sh"]
