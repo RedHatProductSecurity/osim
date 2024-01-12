f3a522977eb36703bfd0bf16e0661396b4f111dd
superbuggy <jpqreichard@gmail.com>
2024-01-12 16:45:21 -0500

    ♻️ Simplify API key settings logic

--- src/services/ApiKeyService.ts
+++ src/services/ApiKeyService.ts
@@ -8,33 +8,32 @@
 
 import {useSettingsStore} from '@/stores/SettingsStore';
 import {useToastStore} from '@/stores/ToastStore';
-import type { ToastNew } from "@/stores/ToastStore";
+import type { ToastNew } from '@/stores/ToastStore';
 
 export function notifyApiKeyUnset() {
+
   const { addToast } = useToastStore();
-  const settingsStore = useSettingsStore();
-  const { bugzillaApiKey, jiraApiKey } = settingsStore.settings;
-
-  const toastOptions: ToastNew = {
-    css: "warning",
-    title: "Request CVE",
-    bodyHtml: true,
-    body: "",
-  };
-
-  const isBugzillaApiKeyUnset = !/.+/.test(bugzillaApiKey ?? "");
-  const isJiraApiKeyUnset = !/.+/.test(jiraApiKey ?? "");
+  const { settings: { bugzillaApiKey, jiraApiKey } } = useSettingsStore();
   const unsetKeys: string[] = [];
 
-  if (isBugzillaApiKeyUnset) {
-    unsetKeys.push("Bugzilla");
+  if (!bugzillaApiKey) {
+    unsetKeys.push('Bugzilla');
   }
-  if (isJiraApiKeyUnset) {
-    unsetKeys.push("JIRA");
+
+  if (!jiraApiKey) {
+    unsetKeys.push('JIRA');
   }
-  if (isBugzillaApiKeyUnset || isJiraApiKeyUnset) {
+
+  if (!bugzillaApiKey || !jiraApiKey) {
     const lis = unsetKeys.map((key) => `<li>${key}</li>`).join('');
-    toastOptions.body += `You have not set the following keys in this tab: <ul>${lis}</ul>Flaw creation requires your Bugzilla API or JIRA key to be set. Visit <a href="/settings"}>Settings</a> to set any required keys.`;
+    const toastOptions: ToastNew = {
+      css: 'warning',
+      title: 'Request CVE',
+      bodyHtml: true,
+      body: '',
+    };
+    toastOptions.body += `You have not set the following keys in this tab: <ul>${lis}</ul>Flaw creation requires your Bugzilla API or JIRA key to be set. Visit <a href='/settings'}>Settings</a> to set any required keys.`;
     addToast(toastOptions);
   }
+
 }
