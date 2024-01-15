55bddbbfbc6e48ced3cf39bda137b4ae7ba67994
superbuggy <jpqreichard@gmail.com>
2024-01-15 16:03:35 -0500

    🔥 Remove dead code, organize imports

--- src/components/IssueEdit.vue
+++ src/components/IssueEdit.vue
@@ -1,35 +1,31 @@
 <script setup lang="ts">
-// import type {Flaw} from '@/generated-client';
+
 import { DateTime } from 'luxon';
-import {computed, reactive, ref} from 'vue';
-import EditableText from '../components/widgets/EditableText.vue';
+import { computed, reactive, ref } from 'vue';
+import { useField, useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+
+import { ZodFlawSchema } from '../types/zodFlaw';
+import type { ZodFlawType } from '../types/zodFlaw';
+
 import LabelEditable from '@/components/widgets/LabelEditable.vue';
-import * as zf from '../types/zodFlaw';
 import LabelSelect from '@/components/widgets/LabelSelect.vue';
 import LabelStatic from '@/components/widgets/LabelStatic.vue';
-import {ZodFlawSchema} from '../types/zodFlaw';
 import LabelTextarea from '@/components/widgets/LabelTextarea.vue';
-import {getFlawBugzillaLink, getFlawOsimLink, postFlawPublicComment, putFlaw} from '@/services/FlawService';
-import type {ZodFlawType} from '../types/zodFlaw';
-import {useField, useForm} from 'vee-validate';
-import {toTypedSchema} from '@vee-validate/zod';
-import {z} from 'zod';
-const router = useRouter();
 import LabelInput from '@/components/widgets/LabelInput.vue';
-import {useRouter} from 'vue-router';
-import {useToastStore} from '@/stores/ToastStore';
-import CveRequestForm from '@/components/CveRequestForm.vue';
-import {getDisplayedOsidbError} from '@/services/OsidbAuthService';
-import PillList from '@/components/widgets/PillList.vue';
-import LabelCheckbox from '@/components/widgets/LabelCheckbox.vue';
 import AffectedOfferingForm from '@/components/AffectedOfferingForm.vue';
-import {postAffect, putAffect} from '@/services/AffectService';
-import {notifyApiKeyUnset} from '@/services/ApiKeyService';
+import CveRequestForm from '@/components/CveRequestForm.vue';
+import PillList from '@/components/widgets/PillList.vue';
+
+import { getFlawBugzillaLink, getFlawOsimLink, postFlawPublicComment, putFlaw } from '@/services/FlawService';
+import { getDisplayedOsidbError } from '@/services/OsidbAuthService';
+import { postAffect, putAffect } from '@/services/AffectService';
+import { notifyApiKeyUnset } from '@/services/ApiKeyService';
+import { useToastStore } from '@/stores/ToastStore';
 
 const {addToast} = useToastStore();
 
 const props = defineProps<{
-  // flaw: Flaw,
   flaw: any,
 }>();
 
@@ -39,14 +35,10 @@ const emit = defineEmits<{
 
 notifyApiKeyUnset();
 
-// const flawTypes = Object.values(ZodFlawSchema.shape.type.unwrap().enum) as string[];
 const flawTypes = Object.values(ZodFlawSchema.shape.type.unwrap().unwrap().enum) as string[];
-// const flawSources = Object.values(ZodFlawSchema.shape.source.unwrap().enum) as string[];
-// const flawImpacts = Object.values(ZodFlawSchema.shape.impact.unwrap().enum) as string[];
 const flawSources = Object.values(ZodFlawSchema.shape.source.unwrap().unwrap().enum) as string[];
 const flawImpacts = Object.values(ZodFlawSchema.shape.impact.unwrap().unwrap().enum) as string[];
 const incidentStates = Object.values(ZodFlawSchema.shape.major_incident_state.unwrap().unwrap().enum) as string[];
-
 
 console.log(flawTypes);
 console.log(flawSources);
@@ -71,26 +63,19 @@ const {value: flawComponent} = useField<string>('component');
 const {value: flawTitle} = useField<string>('title');
 const {value: flawDescription} = useField<string>('description');
 const {value: flawSummary} = useField<string>('summary');
-const {value: flawRequires_summary} = useField<string>('requires_summary');
 const {value: flawStatement} = useField<string>('statement');
 const {value: flawCwe_id} = useField<string>('cwe_id');
 const {value: flawUnembargo_dt} = useField<string>('unembargo_dt');
 const {value: flawSource} = useField<string>('source');
 const {value: flawReported_dt} = useField<string>('reported_dt');
 const {value: flawMitigation} = useField<string>('mitigation');
-const {value: flawCvss2} = useField<string>('cvss2');
-const {value: flawCvss2_score} = useField<number>('cvss2_score');
-const {value: flawNvd_cvss2} = useField<string>('nvd_cvss2');
 const {value: flawCvss3} = useField<string>('cvss3');
 const {value: flawCvss3_score} = useField<number>('cvss3_score');
 const {value: flawNvd_cvss3} = useField<string>('nvd_cvss3');
 const {value: flawMajor_incident_state} = useField<string>('major_incident_state');
-const {value: flawNist_cvss_validation} = useField<string>('nist_cvss_validation');
-const {value: flawEmbargoed} = useField<boolean>('embargoed');
-const {value: flawUpdated_dt} = useField<string>('updated_dt');
 
-const {value: flawAssignee} = useField<string>('assignee'); // TODO: Connect when supported by API
-const {value: flawStatus} = useField<string>('status'); // TODO: Connect when supported by API
+const {value: flawAssignee} = useField<string>('assignee');
+const {value: flawStatus} = useField<string>('status');
 
 let committedFlaw: ZodFlawType = reactive(props.flaw);
 let stagedFlaw: ZodFlawType = committedFlaw;
@@ -162,7 +147,6 @@ const onSubmit = handleSubmit((flaw: ZodFlawType) => {
         flawSaved = true;
         console.log('saved flaw', flaw);
       })
-      // .then(saveAffects)
       .then(() => {
         if (flawSaved) {
           addToast({
@@ -183,33 +167,12 @@ const onSubmit = handleSubmit((flaw: ZodFlawType) => {
   for (let affect of theAffects.value) {
     console.log('saving the affect', affect);
   }
-
-  // Promise.reject('success')
-  //     .then(() => {
-  //       console.log('saved flaw', flaw);
-  //       committedFlaw = flaw;
-  //       // router.go(0); // refresh the current route, refreshing the loaded flaw
-  //       addToast({title: 'Info', body: 'Flaw Saved'});
-  //     })
 });
 
 const onReset = (payload: MouseEvent) => {
   console.log('onReset');
   setValues(committedFlaw);
 }
-
-// const errors = {
-//   type: '',
-//   source: '',
-//   cve_id: '',
-//   impact: '',
-//   cwe_id: '',
-//   cvss3: '',
-//   cvss3_score: '',
-//   nvd_cvss3: '',
-//   reported_dt: '',
-//   unembargo_dt: '',
-// };
 
 props.flaw.reported_dt = DateTime.fromISO(props.flaw.reported_dt, { zone: 'utc' }).toJSDate();
 props.flaw.unembargo_dt = DateTime.fromISO(props.flaw.unembargo_dt, { zone: 'utc' }).toJSDate();
@@ -231,43 +194,6 @@ const trackerUuids = computed(() => {
         };
       })
 });
-
-
-// function onsubmit() {
-//
-//   const theFlaw: ZodFlawType = {
-//     type: props.flaw.type,
-//     cve_id: props.flaw.cve_id                               ,
-//     impact: props.flaw.impact                               ,
-//     component: props.flaw.component                         ,
-//     title: props.flaw.title                                 ,
-//     description: props.flaw.description                     ,
-//     summary: props.flaw.summary                             ,
-//     requires_summary: props.flaw.requires_summary           ,
-//     statement: props.flaw.statement                         ,
-//     cwe_id: props.flaw.cwe_id                               ,
-//     unembargo_dt: props.flaw.unembargo_dt                   ,
-//     source: props.flaw.source                               ,
-//     reported_dt: props.flaw.reported_dt                     ,
-//     mitigation: props.flaw.mitigation                       ,
-//     cvss2: props.flaw.cvss2                                 ,
-//     cvss2_score: props.flaw.cvss2_score                     ,
-//     nvd_cvss2: props.flaw.nvd_cvss2                         ,
-//     cvss3: props.flaw.cvss3                                 ,
-//     cvss3_score: props.flaw.cvss3_score                     ,
-//     nvd_cvss3: props.flaw.nvd_cvss3                         ,
-//     major_incident_state: props.flaw.major_incident_state   ,
-//     nist_cvss_validation: props.flaw.nist_cvss_validation   ,
-//     embargoed: props.flaw.embargoed                         ,
-//     // updated_dt: props.flaw.updated_dt                       ,
-//     updated_dt: moment.utc().toISOString()                  ,
-//   }
-//
-//   putFlaw(props.flaw.uuid, theFlaw)
-//       .then(() => {
-//         location.reload(); // TODO extremely ugly hack
-//       })
-// }
 
 function onreset() {
   // TODO FIX
@@ -293,7 +219,6 @@ function addPublicComment() {
 }
 
 function addBlankAffect() {
-  // props.flaw.affects = props.flaw.affects || [];
   theAffects.value.push({});
 }
 function removeAffect(affectIdx: number) {
@@ -307,12 +232,6 @@ function removeAffect(affectIdx: number) {
       class="osim-content container"
       @submit="onSubmit"
   >
-    <!--<div class="row">-->
-    <!--  <ul class="nav nav-tabs">-->
-    <!--    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Issue Details</a></li>-->
-    <!--    <li class="nav-item"><a href="#" class="nav-link">Raw View</a></li>-->
-    <!--  </ul>-->
-    <!--</div>-->
 
     <div class="row mt-3 mb-5">
       <LabelEditable v-model="flawTitle"  label="Title" type="text"/>
@@ -434,21 +353,7 @@ function removeAffect(affectIdx: number) {
         </div>
 
       </div>
-
-<!--      <div class="row">-->
-<!--<pre>-->
-<!--Staged local values:-->
-<!--{{ JSON.stringify(values, null, 2) }}-->
-<!--</pre>-->
-<!--<pre>-->
-<!--Committed values on server:-->
-<!--{{ JSON.stringify(committedFlaw, null, 2) }}-->
-<!--</pre>-->
-<!--      </div>-->
-
-<!--      <hr class="mt-5">-->
     </div>
-<!--    <div class="row row-cols-5 osim-action-buttons g-3 pb-5 sticky-bottom align-content-end d-flex ">-->
     <div class="osim-action-buttons sticky-bottom d-grid gap-2 d-flex justify-content-end">
       <!--        <button type="button" class="btn btn-primary col">Customer Pending</button>-->
       <!--        <button type="button" class="btn btn-primary col">Close this issue without actions</button>-->
