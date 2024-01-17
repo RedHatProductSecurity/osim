b71eb025f1e3bbd25d7af0acc246d15719cb5efd
Sophia Wang <huiwang@redhat.com>
2024-01-17 17:53:56 +0800

    Add public comment

--- features/locators.py
+++ features/locators.py
@@ -3,6 +3,7 @@ FLAW_CHECKBOX = "input[class='form-check-input']"
 FLAW_FILTER = "input[placeholder='Filter Issues/Flaws']"
 FLAW_LIST = "div[class='osim-incident-list']"
 USER_BUTTON = "button[class='btn btn-secondary dropdown-toggle osim-user-profile']"
+FLAW_INDEX = "ul[class='navbar-nav me-auto align-items-center'] li:nth-child(1) a"
 
 # Locators by xpath
 COMMENT_BUTTON = "//button[contains(text(), 'Comment')]"
