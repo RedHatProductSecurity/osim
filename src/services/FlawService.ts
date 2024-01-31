9f734fcb78432477da31f095907e2e0141d46db8
superbuggy <jpqreichard@gmail.com>
2024-01-31 17:26:40 -0500

    🥅 Handle error, kind of

--- src/services/FlawService.ts
+++ src/services/FlawService.ts
@@ -159,8 +159,11 @@ export async function searchFlaws(query: string) {
     params: {
       search: query,
     },
-  }).then(response => {
-    return response.data;
+  }).then(response => response.data)
+  .catch(error => {
+    // TODO: import {getDisplayedOsidbError} from '@/services/OsidbAuthService';
+    console.error(error);
+    return error;
   });
 }
 
