671f8cace1334566346e001a22a3ea99f4cdc54e
superbuggy <jpqreichard@gmail.com>
2024-01-31 12:56:12 -0500

    🎨 Revert formatting

--- src/components/IssueSearch.vue
+++ src/components/IssueSearch.vue
@@ -6,7 +6,6 @@ import { searchFlaws } from '../services/FlawService';
 import IssueQueueItem from '../components/IssueQueueItem.vue';
 import IssueSearchAdvanced from '../components/IssueSearchAdvanced.vue';
 
-
 type FilteredIssue = {
   issue: any;
   selected: boolean;
@@ -20,15 +19,19 @@ let issueFilter = ref('');
 
 let filteredIssues = computed<FilteredIssue[]>(() => {
   if (issueFilter.value.length === 0) {
-    return issues.value.map(issue => reactive({ issue: issue, selected: false }));
+    return issues.value.map((issue) =>
+      reactive({ issue: issue, selected: false })
+    );
   }
   const filterCaseInsensitive = issueFilter.value.toLowerCase();
   return issues.value
     .filter((issue: any) => {
       // return [issue.title, issue.cve_id, issue.state, issue.source].join(' ').toLowerCase().includes(issueFilter.value.toLowerCase());
-      return [issue.title, issue.cve_id, issue.state, issue.source].some(text => text && text.toLowerCase().includes(filterCaseInsensitive));
+      return [issue.title, issue.cve_id, issue.state, issue.source].some(
+        (text) => text && text.toLowerCase().includes(filterCaseInsensitive)
+      );
     })
-    .map(issue => reactive({ issue: issue, selected: false }));
+    .map((issue) => reactive({ issue: issue, selected: false }));
 });
 
 function updateSelectAll(selectedAll: boolean) {
@@ -41,12 +44,14 @@ let isSelectAllIndeterminate = computed(() => {
   if (filteredIssues.value.length === 0) {
     return false;
   }
-  return filteredIssues.value.some((it) => it.selected !== filteredIssues.value[0].selected)
-})
+  return filteredIssues.value.some(
+    (it) => it.selected !== filteredIssues.value[0].selected
+  );
+});
 
 let isSelectAllChecked = computed(() => {
-  return filteredIssues.value.every(it => it.selected);
-})
+  return filteredIssues.value.every((it) => it.selected);
+});
 
 const searchQuery = z.object({
   query: z.object({
@@ -62,7 +67,7 @@ onMounted(() => {
       return;
     }
     searchFlaws(parsedRoute.query.query)
-      .then(response => setIssues(response.results))
+      .then((response) => setIssues(response.results))
       .catch(console.error);
   } catch (e) {
     console.log('IssueSearch: error advanced searching', e);
@@ -77,10 +82,10 @@ function setIssues(loadedIssues: []) {
 <template>
   <div class="osim-content container">
     <div class="osim-incident-filter">
-      <div class="col-4 mt-4">
+      <div class="col-lg-6 col-md-8 mt-4">
         <IssueSearchAdvanced :setIssues="setIssues" />
       </div>
-      <label class="me-3">
+      <label>
         <!--Filter By-->
         <!--<select>-->
         <!--  <option value="Issues assigned to Me">Issues assigned to Me</option>-->
@@ -89,16 +94,26 @@ function setIssues(loadedIssues: []) {
         <!--  <option value="Newest">Newest</option>-->
         <!--</select>-->
 
-        <input type="text" v-model="issueFilter" class="form-text form-control" placeholder="Filter Issues/Flaws" />
+        <input
+          type="text"
+          v-model="issueFilter"
+          class="form-text form-control"
+          placeholder="Filter Issues/Flaws"
+        />
       </label>
     </div>
     <div class="osim-incident-list">
       <table class="table">
         <thead>
           <tr>
-            <th><input type="checkbox" :indeterminate="isSelectAllIndeterminate" :checked="isSelectAllChecked"
+            <th>
+              <input
+                type="checkbox"
+                :indeterminate="isSelectAllIndeterminate"
+                :checked="isSelectAllChecked"
                 @change="updateSelectAll(($event.target as HTMLInputElement).checked)"
-                aria-label="Select All Issues in Table">
+                aria-label="Select All Issues in Table"
+              />
             </th>
             <th>ID</th>
             <th>Impact</th>
@@ -111,7 +126,10 @@ function setIssues(loadedIssues: []) {
         </thead>
         <tbody class="table-group-divider">
           <template v-for="filteredIssue of filteredIssues">
-            <IssueQueueItem :issue="filteredIssue.issue" v-model:selected="filteredIssue.selected" />
+            <IssueQueueItem
+              :issue="filteredIssue.issue"
+              v-model:selected="filteredIssue.selected"
+            />
           </template>
         </tbody>
       </table>
