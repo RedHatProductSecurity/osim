6b3f44df3ed8aeadeda44f4f6b34479095f454c4
superbuggy <jpqreichard@gmail.com>
2024-02-02 18:33:39 -0500

    🐛 Fix broken IssueAdvancedSearch text

--- src/components/__tests__/IssueSearchAdvanced.spec.ts
+++ src/components/__tests__/IssueSearchAdvanced.spec.ts
@@ -1,7 +1,11 @@
-import { describe, it, expect, vi, type Mock } from 'vitest';
 import { mount, VueWrapper } from '@vue/test-utils';
+import { describe, it, expect, vi, type Mock } from 'vitest';
 import { useRoute } from 'vue-router';
-import IssueSearchAdvanced from '../IssueSearchAdvanced.vue';
+import IssueSearchAdvanced from '@/components/IssueSearchAdvanced.vue';
+import { useToastStore } from '@/stores/ToastStore';
+
+import { createTestingPinia } from '@pinia/testing';
+
 
 vi.mock('vue-router', async () => {
   const actual = await vi.importActual("vue-router");
@@ -18,10 +22,22 @@ vi.mock('vue-router', async () => {
 describe('IssueSearchAdvanced', () => {
   let wrapper: VueWrapper<any>;
 
+  beforeAll(() => {
+    createTestingPinia({
+      initialState: {
+        toasts: [],
+      },
+    });
+  })
+
   beforeEach(() => {
     const props: typeof IssueSearchAdvanced.props = {};
     wrapper = mount(IssueSearchAdvanced, {
-      props
+      props,
+      plugins:[useToastStore()],
+      global: {
+        mocks: { useRoute },
+      }
     });
   })
 
