2c3c187db2e9822a5308974da33a44079fb5fb1d
superbuggy <jpqreichard@gmail.com>
2024-02-02 16:07:32 -0500

    💡 Add TODO

--- src/components/widgets/LabelStatic.vue
+++ src/components/widgets/LabelStatic.vue
@@ -11,6 +11,8 @@ defineProps<{
     <span class="form-label">
       {{ label }}
     </span>
+    <!-- TODO: osim-pill-list was implemented for the Promote/Reject buttons, -->
+    <!-- remove this and replace with btn-group for that feature -->
     <div class="osim-pill-list form-control" :class="{ 'alert alert-warning': !modelValue }">
       {{ modelValue }}
     </div>
