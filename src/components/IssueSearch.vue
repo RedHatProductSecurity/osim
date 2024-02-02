4cf0ac43dcb711f5df2a80265777f91b690189d6
d10n <d10n@redhat.com>
2024-02-02 18:07:04 -0500

    Move CVE quick search to nav bar

--- src/components/IssueSearch.vue
+++ src/components/IssueSearch.vue
@@ -4,7 +4,6 @@ import {useRoute} from 'vue-router';
 import {z} from 'zod';
 import {searchFlaws} from '../services/FlawService';
 import IssueQueueItem from '../components/IssueQueueItem.vue';
-import router from '@/router';
 
 type FilteredIssue = {
   issue: any;
@@ -53,25 +52,12 @@ const searchQuery = z.object({
   }),
 });
 
-function quickMatchCVE(query: string) {
-  // Match `CVE-`, 4 digits, a hyphen, then 4-7 digits,
-  // with optional surrounding whitespace.
-  const cveRegex = /^\s*(CVE-\d{4}-\d{4,7})\s*$/
-  // match[1] will be the CVE ID if it exists
-  return query.match(cveRegex)?.[1];
-}
-
 onMounted(() => {
 
   try {
     const parsedRoute = searchQuery.parse(route);
     if (parsedRoute.query.query === '') {
       // TODO handle error
-      return;
-    }
-    const maybeCveId =  quickMatchCVE(parsedRoute.query.query);
-    if (maybeCveId) {
-      router.push({path: `/flaws/${maybeCveId}`});
       return;
     }
     searchFlaws(parsedRoute.query.query)
