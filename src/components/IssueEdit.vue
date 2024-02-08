e7418707b9b6b8afb55958c80bf5c78f3a8ae70d
d10n <d10n@redhat.com>
2024-02-08 12:57:47 -0500

    Extract IssueEdit to be just a v-model

--- src/components/IssueEdit.vue
+++ src/components/IssueEdit.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 import { DateTime } from 'luxon';
-import { computed, reactive, ref } from 'vue';
+import {computed, reactive, ref, toRef} from 'vue';
 import { RouterLink } from 'vue-router';
 import { ZodFlawSchema, type ZodFlawType } from '../types/zodFlaw';
 import LabelEditable from '@/components/widgets/LabelEditable.vue';
@@ -28,11 +28,13 @@ import { useToastStore } from '@/stores/ToastStore';
 const {addToast} = useToastStore();
 
 const props = defineProps<{
-  flaw: any,
+  flaw: any;
+  mode: 'create' | 'edit';
 }>();
 
 const emit = defineEmits<{
-  'refresh:flaw': [],
+  'update:flaw': [flaw: any];
+  'refresh:flaw': [];
 }>();
 
 notifyApiKeyUnset();
@@ -42,7 +44,7 @@ const flawSources = Object.values(ZodFlawSchema.shape.source.unwrap().unwrap().e
 const flawImpacts = Object.values(ZodFlawSchema.shape.impact.unwrap().unwrap().enum) as string[];
 const incidentStates = Object.values(ZodFlawSchema.shape.major_incident_state.unwrap().unwrap().enum) as string[];
 
-const theAffects = ref<any[]>(props.flaw.affects);
+const theAffects = toRef(props.flaw, 'affects');
 
 // TODO
 const errors = {
@@ -60,102 +62,6 @@ const errors = {
   source: null,
 };
 
-let committedFlaw: ZodFlawType = reactive(props.flaw);
-
-const onSubmitAffect = async () => {
-  for (let affect of theAffects.value) {
-    let go = true;
-    console.log('saving the affect', affect);
-    console.log(affect.uuid);
-    const newAffect = {
-      flaw: props.flaw.uuid,
-      type: affect.type,
-      affectedness: affect.affectedness,
-      resolution: affect.resolution,
-      ps_module: affect.ps_module,
-      ps_component: affect.ps_component,
-      impact: affect.impact,
-      embargoed: affect.embargoed || false,
-      updated_dt: affect.updated_dt,
-    }
-    if (!go) {
-      continue;
-    }
-    if (affect.uuid != null) {
-      await putAffect(affect.uuid, newAffect)
-          .then(() => {
-            console.log('saved newAffect', newAffect);
-            addToast({
-              title: 'Info',
-              body: `Affect Saved: ${newAffect.ps_component}`,
-            });
-          })
-          .catch(error => {
-            const displayedError = getDisplayedOsidbError(error);
-            addToast({
-              title: 'Error saving Affect',
-              body: displayedError,
-            });
-            console.log(error);
-          })
-    } else {
-      await postAffect(newAffect)
-          .then(() => {
-            console.log('saved newAffect', newAffect);
-            addToast({
-              title: 'Info',
-              body: `Affect Saved: ${newAffect.ps_component}`,
-            });
-          })
-          .catch(error => {
-            const displayedError = getDisplayedOsidbError(error);
-            addToast({
-              title: 'Error saving Affect',
-              body: displayedError,
-            });
-            console.log(error);
-          })
-    }
-  }
-  emit('refresh:flaw');
-}
-
-const onSubmit = () => {
-  let flaw = props.flaw;
-  let flawSaved = false;
-  // Save Flaw, then safe Affects, then refresh
-  putFlaw(props.flaw.uuid, flaw)
-      .then(() => {
-        flawSaved = true;
-        console.log('saved flaw', flaw);
-      })
-      .then(() => {
-        if (flawSaved) {
-          addToast({
-            title: 'Info',
-            body: 'Flaw Saved',
-          });
-          emit('refresh:flaw');
-        }
-      })
-      .catch(error => {
-        const displayedError = getDisplayedOsidbError(error);
-        addToast({
-          title: 'Error saving Flaw',
-          body: displayedError,
-        });
-        console.log(error);
-      })
-  for (let affect of theAffects.value) {
-    console.log('saving the affect', affect);
-  }
-};
-
-const onReset = (payload: MouseEvent) => {
-  console.log('onReset');
-  // FIXME XXX TODO
-}
-
 const flawCvss3CaculatorLink = computed(()=>{
   let link = props.flaw.cvss3;
   const index = props.flaw.cvss3.indexOf('CVSS');
@@ -165,8 +71,8 @@ const flawCvss3CaculatorLink = computed(()=>{
   return `https://www.first.org/cvss/calculator/3.1#${link}`;
 });
 
-props.flaw.reported_dt = DateTime.fromISO(props.flaw.reported_dt, { zone: 'utc' }).toJSDate();
-props.flaw.unembargo_dt = DateTime.fromISO(props.flaw.unembargo_dt, { zone: 'utc' }).toJSDate();
+// props.flaw.reported_dt = DateTime.fromISO(props.flaw.reported_dt, { zone: 'utc' }).toJSDate();
+// props.flaw.unembargo_dt = DateTime.fromISO(props.flaw.unembargo_dt, { zone: 'utc' }).toJSDate();
 
 console.log('flaw reported_dt', props.flaw.reported_dt);
 
@@ -185,11 +91,6 @@ const trackerUuids = computed(() => {
         };
       })
 });
-
-function onreset() {
-  // TODO FIX
-}
-
 
 function addPublicComment() {
   postFlawPublicComment(props.flaw.uuid, newPublicComment.value)
@@ -220,15 +121,12 @@ function removeAffect(affectIdx: number) {
 </script>
 
 <template>
-  <form
-      class="osim-content container"
-      @submit="onSubmit()"
-  >
-  <div class="row mt-3 mb-5">
-    <LabelEditable v-model="flaw.title"  label="Title" type="text"/>
-    <div class="col">
-      <div class="row">
-        <div class="col-6">
+  <div class="osim-content container">
+    <div class="row mt-3 mb-5">
+      <LabelEditable v-model="flaw.title" label="Title" type="text"/>
+      <div class="col">
+        <div class="row">
+          <div class="col-6">
             <LabelEditable label="Component" type="text" v-model="flaw.component" :error="errors.component"/>
             <LabelSelect label="Type" :options="flawTypes" v-model="flaw.type" :error="errors.type"/>
             <div class="">
@@ -237,7 +135,8 @@ function removeAffect(affectIdx: number) {
                   <LabelEditable
                       label="CVE ID" type="text" v-model="flaw.cve_id" :error="errors.cve_id"/>
                 </div>
-                <div v-if="!(flaw.cve_id || '').includes('CVE')" class="col-auto align-self-end mb-3">
+                <div v-if="!(flaw.cve_id || '').includes('CVE') && mode === 'edit'"
+                     class="col-auto align-self-end mb-3">
                   <CveRequestForm
                       :bugzilla-link="getFlawBugzillaLink(flaw)"
                       :osim-link="getFlawOsimLink(flaw.uuid)"
@@ -259,8 +158,9 @@ function removeAffect(affectIdx: number) {
             <LabelSelect label="Source" :options="flawSources" v-model="flaw.source" :error="errors.source"/>
           </div>
           <div class="col-6">
-            <IssueFieldStatus :classification="flaw.classification" :flawId="flaw.uuid"/>
-            <LabelSelect label="Incident State" :options="incidentStates" v-model="flaw.major_incident_state" :error="errors.major_incident_state"/>
+            <IssueFieldStatus v-if="mode === 'edit'" :classification="flaw.classification" :flawId="flaw.uuid"/>
+            <LabelSelect label="Incident State" :options="incidentStates" v-model="flaw.major_incident_state"
+                         :error="errors.major_incident_state"/>
             <LabelEditable label="Reported Date" type="date" v-model="flaw.reported_dt" :error="errors.reported_dt"/>
             <LabelEditable
                 :label="'Public Date' + (DateTime.fromJSDate(flaw.unembargo_dt).diffNow().milliseconds > 0 ? ' [FUTURE]' : '')"
@@ -270,8 +170,8 @@ function removeAffect(affectIdx: number) {
             <IssueFieldEmbargo
                 v-model="flaw.embargoed"
                 :cveId="flaw.cve_id"/>
-            <LabelEditable label="Assignee" type="text" v-model="flaw.owner" />
-            <LabelEditable type="text" label="Team ID" v-model="flaw.team_id" />
+            <LabelEditable label="Assignee" type="text" v-model="flaw.owner"/>
+            <LabelEditable type="text" label="Team ID" v-model="flaw.team_id"/>
             <div>
               <div v-if="flaw.trackers && flaw.trackers.length > 0">Trackers:</div>
               <div v-for="tracker in trackerUuids">
@@ -317,8 +217,15 @@ function removeAffect(affectIdx: number) {
       <div class="row">
         <ul>
           <li class="p-3" v-for="comment in flaw.comments">
-            <p class="border-top pt-2">{{comment.meta_attr.creator}} / <a :href="'#' + comment.type + '/' + comment.external_system_id">{{comment.meta_attr.time}}</a> <span v-if="comment.meta_attr.is_private.toLowerCase() === 'true'">(internal)</span></p>
-            <p>{{comment.meta_attr.text}}</p>
+            <p class="border-top pt-2">
+              {{ comment.meta_attr.creator }} /
+              <a :href="'#' + comment.type + '/' + comment.external_system_id">
+                {{ comment.meta_attr.time }}
+              </a>
+              <span v-if="comment.meta_attr.is_private.toLowerCase() === 'true'"
+              >(internal)</span>
+            </p>
+            <p>{{ comment.meta_attr.text }}</p>
           </li>
         </ul>
         <div v-if="!addComment">
@@ -329,7 +236,7 @@ function removeAffect(affectIdx: number) {
           >Add Public Comment</button>
         </div>
         <div v-if="addComment">
-        <LabelTextarea label="New Public Comment" v-model="newPublicComment"/>
+          <LabelTextarea label="New Public Comment" v-model="newPublicComment"/>
           <button
               type="button"
               @click="addPublicComment"
@@ -340,24 +247,7 @@ function removeAffect(affectIdx: number) {
 
       </div>
     </div>
-    <div class="osim-action-buttons sticky-bottom d-grid gap-2 d-flex justify-content-end">
-      <!--        <button type="button" class="btn btn-primary col">Customer Pending</button>-->
-      <!--        <button type="button" class="btn btn-primary col">Close this issue without actions</button>-->
-      <!--        <button type="button" class="btn btn-primary col">Move this issue to another source queue</button>-->
-      <!--        <button type="button" class="btn btn-primary col">Create a flaw</button>-->
-      <!--        <button type="button" class="btn btn-primary col">Create hardening bug/weakness</button>-->
-      <button
-          type="button"
-          class="btn btn-secondary"
-          @click="onReset"
-      >Reset Changes</button>
-      <button
-          type="submit"
-          class="btn btn-primary"
-          @click="onSubmit"
-      >Save Changes</button>
-    </div>
-  </form>
+  </div>
 </template>
 
 <style scoped>
@@ -385,21 +275,6 @@ function removeAffect(affectIdx: number) {
 .affect-trackers-heading {
   font-size: 100%;
   font-weight: bold;
-}
-
-
-.osim-content.container label {
-  display: block;
-}
-
-.osim-action-buttons {
-  background: white;
-  border-top: 1px solid #ddd;
-  margin-left: -20px;
-  margin-right: -20px;
-  padding-right: 20px;
-  padding-bottom: 2rem;
-  padding-top: 0.5rem;
 }
 
 .osim-affects {
