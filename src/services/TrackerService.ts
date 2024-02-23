3d7bb8a79010e8a9026638bc1736cf7a0da59953
superbuggy <jpqreichard@gmail.com>
2024-02-22 21:40:57 -0500

    ✨ Define tracker REST behavior

--- src/services/TrackerService.ts
+++ src/services/TrackerService.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import {osidbFetch} from '@/services/OsidbAuthService';
+import { osidbFetch } from '@/services/OsidbAuthService';
 
 export async function getTrackers() {
   if (import.meta.env.VITE_RUNTIME_LEVEL === 'MOCK') {
-    return axios.get('/mock/new-trackers-stage.json')
+    return axios.get('/mock/new-trackers-stage.json');
   }
   if (import.meta.env.VITE_RUNTIME_LEVEL === 'DEV') {
     // return osidbFetch({
@@ -14,7 +14,8 @@ export async function getTrackers() {
       method: 'get',
       url: '/osidb/api/v1/trackers',
       params: {
-        include_fields: 'cve_id,uuid,impact,source,created_dt,updated_dt,classification,is_major_incident,title,state,unembargo_dt',
+        include_fields:
+          'cve_id,uuid,impact,source,created_dt,updated_dt,classification,is_major_incident,title,state,unembargo_dt',
       },
     });
   }
@@ -22,7 +23,8 @@ export async function getTrackers() {
     method: 'get',
     url: '/osidb/api/v1/trackers',
     params: {
-      include_fields: 'cve_id,uuid,impact,source,created_dt,updated_dt,classification,is_major_incident,title,state,unembargo_dt',
+      include_fields:
+        'cve_id,uuid,impact,source,created_dt,updated_dt,classification,is_major_incident,title,state,unembargo_dt',
     },
   });
   // if (import.meta.env.VITE_RUNTIME_LEVEL === 'PROD') {
@@ -32,17 +34,34 @@ export async function getTrackers() {
 
 export async function getTracker(uuid: string) {
   if (import.meta.env.VITE_RUNTIME_LEVEL === 'MOCK') {
-    return axios.get('/mock/new-trackers-stage.json')
-        .then(response => {
-          const tracker = response.data.results.find((tracker: { uuid: string; }) => tracker.uuid === uuid);
-          return tracker;
-        })
+    return axios.get('/mock/new-trackers-stage.json').then((response) => {
+      const tracker = response.data.results.find(
+        (tracker: { uuid: string }) => tracker.uuid === uuid,
+      );
+      return tracker;
+    });
   }
 
   return osidbFetch({
     method: 'get',
-    url: `/osidb/api/v1/trackers/${uuid}`
-  }).then(response => {
+    url: `/osidb/api/v1/trackers/${uuid}`,
+  }).then((response) => {
     return response.data;
-  })
+  });
+}
+
+type TrackersPost = {
+  affects: string[];
+  ps_update_stream: string;
+  resolution: string;
+  embargoed: boolean;
+  updated_dt: string; //
+};
+
+export async function fileTracker(uuid: string, requestBody: TrackersPost) {
+  return osidbFetch({
+    method: 'post',
+    url: '/osidb/api/v1/trackers',
+    data: requestBody,
+  });
 }
