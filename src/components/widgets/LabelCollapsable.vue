c00852cdf9b0bd7c0b39b5fd01b339f1456be12f
superbuggy <jpqreichard@gmail.com>
2024-03-01 00:28:34 -0500

    ✨ Allow local state management for collapsible

--- src/components/widgets/LabelCollapsable.vue
+++ src/components/widgets/LabelCollapsable.vue
@@ -1,22 +1,35 @@
 <script setup lang="ts">
-const props = defineProps<{
-  label?: string;
-  isExpanded: boolean;
-}>();
+import { ref } from 'vue';
+const props = withDefaults(
+  defineProps<{
+    label?: string;
+    isExpanded?: boolean | null;
+  }>(),
+  {
+    isExpanded: null,
+    label: '',
+  },
+);
 
+const isExpandedLocal = ref(false);
 const emit = defineEmits<{
-  'toggleExpanded': [value: boolean],
+  toggleExpanded: [value: boolean];
 }>();
 
 function handleClick() {
-  emit('toggleExpanded', !props.isExpanded);
+  console.log(props.isExpanded);
+  if (props.isExpanded !== null) {
+    emit('toggleExpanded', !props.isExpanded);
+  } else {
+    isExpandedLocal.value = !isExpandedLocal.value;
+  }
 }
 </script>
 
 <template>
   <div class="osim-collapsable-label" v-bind="$attrs">
     <button type="button" class="me-2" @click="handleClick">
-      <i v-if="isExpanded" class="bi bi-dash-square-dotted me-1"></i>
+      <i v-if="isExpanded || isExpandedLocal" class="bi bi-dash-square-dotted me-1"></i>
       <i v-else class="bi bi-plus-square-dotted me-1"></i>
       <slot name="label">
         <label class="ms-2 form-label">
@@ -26,7 +39,7 @@ function handleClick() {
     </button>
     <slot name="buttons" />
     <div class="ps-3 border-start">
-      <div :class="{ 'visually-hidden': !isExpanded }">
+      <div :class="{ 'visually-hidden': !(isExpanded || isExpandedLocal) }">
         <slot />
       </div>
     </div>
@@ -38,6 +51,10 @@ function handleClick() {
   border: none;
   padding: 0;
   background: transparent;
+}
+
+.osim-collapsable-label button * {
+  cursor: pointer;
 }
 
 .osim-collapsable-label :deep(div.osim-static-label),
