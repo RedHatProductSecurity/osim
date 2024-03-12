e06c6631ad4978d0c25e8b567c04867be7b0e152
superbuggy <jpqreichard@gmail.com>
2024-03-12 13:18:08 -0400

    ✨ Make comments filterable

--- src/components/FlawComments.vue
+++ src/components/FlawComments.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import LabelTextarea from '@/components/widgets/LabelTextarea.vue';
 
-defineProps<{
+const props = defineProps<{
   comments: any[];
 }>();
 const newPublicComment = ref('');
@@ -17,39 +17,128 @@ function handleClick() {
   isAddCommentShown.value = false;
 }
 
+function parseGroups(serializedJson: string) {
+  try {
+    return JSON.parse(serializedJson.replace(/'/g, '\\"'));
+  } catch (e) {
+    return [];
+  }
+}
+
+const transformedComments = computed(() =>
+  props.comments.map((comment) => ({
+    ...comment,
+    meta_attr: {
+      ...comment.meta_attr,
+      is_private: (comment.meta_attr?.is_private || '').toLowerCase() === 'true',
+      private_groups: parseGroups(comment.meta_attr?.private_groups),
+    },
+  })),
+);
+
+type CommentFilter = 'public' | 'private' | 'system';
+type CommentActiveFilters = Record<CommentFilter, boolean>;
+type CommentFilterFunctions = Record<CommentFilter, (comment: any) => boolean>;
+
+const selectedFilters = ref<CommentActiveFilters>({
+  public: true,
+  private: false,
+  system: false,
+});
+
+const filterFunctions: CommentFilterFunctions = {
+  public: (comment: any) => !comment.meta_attr.is_private,
+  private: (comment: any) => comment.meta_attr.is_private,
+  system: (comment: any) => comment.meta_attr.creator === 'bugzilla@redhat.com',
+};
+
+const activeFilters = computed(() => {
+  const activeFilterFunctions = Object.entries(selectedFilters.value)
+    .filter(([, isActive]) => isActive)
+    .map(([filter]) => filterFunctions[filter as CommentFilter]);
+
+  return (comment: any) =>
+    activeFilterFunctions.reduce(
+      (result, filterFunction) => result || filterFunction(comment),
+      false,
+    );
+});
+
+const filteredComments = computed(() => transformedComments.value.filter(activeFilters.value));
+
+const filters: CommentFilter[] = ['public', 'private', 'system'];
 </script>
 
 <template>
-  <h4 class="mt-3 mb-2">Comments</h4>
-  <div class="row">
-    <ul class="col-6">
-      <li v-for="(comment, commentIndex) in comments" :key="commentIndex" class="p-3">
-        <p class="border-top pt-2">
-          <span
-            v-if="(comment.meta_attr?.is_private || '').toLowerCase() === 'true'"
-            class="badge bg-warning rounded-pill"
-          >
-            Bugzilla Internal
-          </span>
-          {{ comment.meta_attr?.creator }} /
-          <a :href="'#' + comment.type + '/' + comment.external_system_id">
-            {{ comment.meta_attr?.time }}
-          </a>
-        </p>
-        <p>{{ comment.meta_attr?.text }}</p>
-      </li>
-    </ul>
-    <div v-if="!isAddCommentShown">
-      <button type="button" class="btn btn-secondary col" @click="isAddCommentShown = true">
-        Add Public Comment
+  <section class="osim-comments">
+    <h4 class="mt-3 mb-2">Comments</h4>
+    <header class="nav">
+      <button
+        v-for="filter in filters"
+        :key="filter"
+        type="button"
+        class="btn osim-comment-filter me-2"
+        :class="{
+          'btn-warning ': selectedFilters[filter],
+          'btn-secondary': !selectedFilters[filter],
+        }"
+        @click="selectedFilters[filter] = !selectedFilters[filter]"
+      >
+        <div class="form-check form-switch">
+          <input
+            id=""
+            type="checkbox"
+            class="form-check-input warning"
+            :checked="selectedFilters[filter]"
+          />
+          {{ filter }}
+        </div>
       </button>
+    </header>
+    <div class="row">
+      <ul class="col-6">
+        <li
+          v-for="(comment, commentIndex) in filteredComments"
+          :key="commentIndex"
+          class="bg-light p-2 mt-3 rounded-2"
+        >
+          <p class="border-bottom pb-2">
+            <span v-if="comment.meta_attr.is_private" class="badge bg-warning rounded-pill">
+              Bugzilla Internal
+            </span>
+            {{ comment.meta_attr?.creator }} /
+            <a :href="'#' + comment.type + '/' + comment.external_system_id">
+              {{ comment.meta_attr?.time }}
+            </a>
+          </p>
+          <p>{{ comment.meta_attr?.text }}</p>
+        </li>
+      </ul>
+      <div v-if="!isAddCommentShown">
+        <button type="button" class="btn btn-secondary col" @click="isAddCommentShown = true">
+          Add Public Comment
+        </button>
+      </div>
+      <div v-if="isAddCommentShown">
+        <LabelTextarea v-model="newPublicComment" label="New Public Comment" />
+        <button type="button" class="btn btn-primary col" @click="handleClick">
+          Add Public Comment
+        </button>
+        <!--<button type="button" class="btn btn-primary col">Add Private Comment</button>-->
+      </div>
     </div>
-    <div v-if="isAddCommentShown">
-      <LabelTextarea v-model="newPublicComment" label="New Public Comment" />
-      <button type="button" class="btn btn-primary col" @click="handleClick">
-        Add Public Comment
-      </button>
-      <!--<button type="button" class="btn btn-primary col">Add Private Comment</button>-->
-    </div>
-  </div>
+  </section>
 </template>
+
+<style scoped lang="scss">
+button.osim-comment-filter {
+  text-transform: capitalize;
+}
+
+section.osim-comments {
+  li {
+    list-style-type: none;
+    background-color: red;
+  }
+}
+</style>
