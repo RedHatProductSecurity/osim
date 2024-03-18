e24620b2ae0ba4787a786d9bcf3515691f1424da
superbuggy <jpqreichard@gmail.com>
2024-03-18 10:55:28 -0400

    🚧 Disable service workers

--- src/stores/SettingsStore.ts
+++ src/stores/SettingsStore.ts
@@ -1,14 +1,15 @@
-import {ref, watch} from 'vue';
+// import {ref, watch} from 'vue';
+import {ref} from 'vue';
 import {defineStore} from 'pinia';
 import {z} from 'zod';
 
-import serviceWorkerClient from '../services/service-worker-client';
+// import serviceWorkerClient from '../services/service-worker-client';
 
 
 // Consideration: we may want some settings to be persistently stored,
 //   but other settings must be in-memory only, e.g. api keys
 
-const _settingsStoreKey = 'SettingsStore';
+// const _settingsStoreKey = 'SettingsStore';
 
 
 export const SettingsSchema = z.object({
@@ -30,18 +31,18 @@ export type SettingsType = z.infer<typeof SettingsSchema>;
 export const useSettingsStore = defineStore('SettingsStore', () => {
   const settings = ref<SettingsType>({showNotifications: false});
   // const settings = useSessionStorage(_settingsStoreKey, {} as SettingsType);
-  serviceWorkerClient.listen(_settingsStoreKey, value => {
-    const newSettingsStore = SettingsSchema.safeParse(value);
-    if (newSettingsStore.success) {
-      if (JSON.stringify(newSettingsStore.data) !== JSON.stringify(settings.value)) {
-        // New value; update
-        settings.value = newSettingsStore.data;
-      }
-    }
-  });
-  watch(settings, () => {
-    serviceWorkerClient.put(_settingsStoreKey, JSON.parse(JSON.stringify(settings.value)));
-  });
+  // serviceWorkerClient.listen(_settingsStoreKey, value => {
+  //   const newSettingsStore = SettingsSchema.safeParse(value);
+  //   if (newSettingsStore.success) {
+  //     if (JSON.stringify(newSettingsStore.data) !== JSON.stringify(settings.value)) {
+  //       // New value; update
+  //       settings.value = newSettingsStore.data;
+  //     }
+  //   }
+  // });
+  // watch(settings, () => {
+  //   serviceWorkerClient.put(_settingsStoreKey, JSON.parse(JSON.stringify(settings.value)));
+  // });
 
   function save(newSettings: SettingsType) {
     settings.value = {...newSettings};
