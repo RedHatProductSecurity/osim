605cef63b5579460113a38de91cdb21bbb61f5b2
superbuggy <jpqreichard@gmail.com>
2024-03-20 10:11:31 -0400

    🔧 Use Istanbul for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,17 +2,19 @@ import { fileURLToPath } from 'node:url';
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      globals: true,
-      globalSetup: [
-        // './src/__tests__/global-setup.ts',
-      ],
+const vitestConfig = defineConfig({
+  test: {
+    coverage: {
+      provider: 'istanbul',
     },
-  }),
-);
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    globals: true,
+    globalSetup: [
+      // './src/__tests__/global-setup.ts',
+    ],
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
