755eb7d6eeb55c6d4faa00e44f731f528e00d06a
Qingmin Duanmu <qduanmu@redhat.com>
2024-03-25 12:32:06 +0800

    Update features to add flaw creation with mandatory data

---
+++ features/steps/flaw_create.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from behave import when, then
+
+from features.utils import (
+    generate_cve,
+    generate_random_text,
+    go_to_first_flaw_detail_page
+)
+from features.pages.flaw_detail_page import FlawDetailPage
+from features.pages.advanced_search_page import AdvancedSearchPage
+from features.utils import go_to_advanced_search_page
+
+
+MAX_RETRY = 10
+
+@given('I open the flaw create page')
+def step_impl(context):
+    flaw_page = FlawDetailPage(context.browser)
+    flaw_page.click_btn('createFlawLink')
+
+
+@when('I fill flaw form with valid data and click create new flaw')
+def step_impl(context):
+    flaw_page = FlawDetailPage(context.browser)
+    context.cve_id = generate_cve()
+    description = generate_random_text()
+    reported_date = datetime.today().strftime("%Y%m%d")
+    flaw_page.set_input_field('title', generate_random_text())
+    flaw_page.set_input_field('component', 'autocomponent')
+    flaw_page.set_input_field('cveid', context.cve_id)
+    flaw_page.set_input_field('reportedDate', reported_date)
+    flaw_page.set_select_value('impact')
+    flaw_page.set_input_field('publicDate', reported_date)
+    flaw_page.click_btn('documentTextFieldsDropDownBtn')
+    flaw_page.set_document_text_field('description', generate_random_text())
+    flaw_page.set_select_value('source')
+    count = 0
+    while count < MAX_RETRY:
+        flaw_page.click_btn('createNewFlawBtn')
+        try:
+            flaw_page.wait_msg('flawCreatedMsg')
+        except Exception:
+            context.cve_id = generate_cve()
+            flaw_page.set_input_field('cveid', context.cve_id)
+            count += 1
+        else:
+            break
+
+
+@then('A new flaw is created')
+def step_impl(context):
+    go_to_advanced_search_page(context.browser)
+    advanced_search_page = AdvancedSearchPage(context.browser)
+    advanced_search_page.select_field_and_value_to_search('cve_id', context.cve_id)
+    advanced_search_page.click_search_btn()
+    advanced_search_page.first_flaw_exist()
+    context.browser.quit()
