1dd79a14f6857b498edceb72f6aae91dd157a6bf
d10n <d10n@redhat.com>
2024-05-01 09:15:15 -0400

    Update column widths

--- src/components/IssueQueueItem.vue
+++ src/components/IssueQueueItem.vue
@@ -9,6 +9,7 @@ const props = defineProps<{
 
 // Temporarily hiding 'Source' column to avoid displaying incorrect information.
 // TODO: unhide it once final issue sources are defined. [OSIDB-2424]
+//       and update the CSS for the column width in IssueQueue
 // const nonIdFields = ['impact', 'source', 'formattedDate', 'title', 'workflowState', 'owner'];
 const nonIdFields = ['impact', 'formattedDate', 'title', 'workflowState', 'owner'];
 
