dbb744326f04819d3d1de4e0f40d4132ec014ce1
Jakub Frejlach <jfrejlac@redhat.com>
2024-05-20 15:30:58 +0200

    Implement FlawAlerts Section

---
+++ src/components/FlawAlertsSection.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { type ZodAlertType } from '@/types/zodShared';
+import FlawAlert from '@/components/FlawAlert.vue';
+import LabelCollapsible from './widgets/LabelCollapsible.vue';
+
+const props = defineProps<{
+  alertSet: Record<string, ZodAlertType[]>;
+  sectionName: string;
+}>();
+
+const isExpanded = ref(false);
+
+</script>
+
+<template>
+  <LabelCollapsible
+    v-if="Object.values(alertSet).reduce((acc, alertType) => acc + alertType.length, 0) !== 0"
+    :isExpanded="isExpanded"
+    @setExpanded="isExpanded = !isExpanded"
+  >
+    <template #label>
+      <span>
+        <label class="mx-2 mb-2">
+          {{ props.sectionName + ":" }}
+        </label>
+        <span v-if="!isExpanded">
+          <span v-if="alertSet['ERROR'].length" class="badge text-bg-danger mx-1">
+            <i class="bi bi-x-circle-fill" />
+            {{ alertSet['ERROR'].length }} Errors
+          </span>
+          <span v-if="alertSet['WARNING'].length" class="badge text-bg-warning mx-1">
+            <i class="bi bi-exclamation-triangle-fill" />
+            {{ alertSet['WARNING'].length }} Warnings
+          </span>
+        </span>
+      </span>
+    </template>
+
+    <div v-for="alerts,alertType in alertSet" :key="alertType">
+      <div v-if="alerts.length">
+        <span v-for="alert in alerts" :key="alert.uuid">
+          <FlawAlert :alert="alert" />
+        </span>
+      </div>
+    </div>
+  </LabelCollapsible>
+</template>
