eabc099da3d1f2942615734fab5d57f4aff8996f
d10n <d10n@redhat.com>
2024-05-29 08:28:19 -0400

    Add extra proxy headers, work around nginx resolver

--- build/entrypoint.d/30-reverse-proxy.sh
+++ build/entrypoint.d/30-reverse-proxy.sh
@@ -8,8 +8,10 @@ fi
 # Get proxy certificate
 curl --xattr $OSIM_NGINX_PROXY_CA -o /tmp/Proxy-CA.crt
 
-# Ensure no trailing slash in variable
+# Ensure no trailing slash in variable, so no duplicate trailing slash is added in proxy_pass
 OSIM_NGINX_PROXY_JIRA="${OSIM_NGINX_PROXY_JIRA%/}"
+
+echo resolver "$(awk -v ORS=' ' '$1=="nameserver" {print $2}' /etc/resolv.conf)" ";" >/etc/nginx/conf.d/resolvers.conf
 
 # Add JIRA reverse proxy endpoint
 cat <<EOF >/tmp/osim-nginx-proxy.conf
@@ -20,6 +22,10 @@ location /proxy/jira/ {
     proxy_ssl_trusted_certificate /tmp/Proxy-CA.crt;
     proxy_ssl_session_reuse on; # try off if connection issues
     proxy_ssl_server_name on; # required
-
+EOF
+cat <<'EOF' >>/tmp/osim-nginx-proxy.conf
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto https;
 }
 EOF
