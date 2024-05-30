a3040ba75326e0e9ae9b43624ae52e9b7106ad7b
Sophia Wang <huiwang@redhat.com>
2024-05-30 14:05:06 +0800

    Uncomment the kinit part

--- selenium/entrypoint.d/30-kinit.sh
+++ selenium/entrypoint.d/30-kinit.sh
@@ -3,13 +3,14 @@
 set -x
 set -e
 
-cleanup() { rm -f /tmp/krb5.keytab; }
-trap cleanup EXIT
-sudo install -o"$(id -u)" -g"$(id -g)" -m400 /keytabs/krb5.keytab /tmp/krb5.keytab
+#cleanup() { rm -f /tmp/krb5.keytab; }
+#trap cleanup EXIT
+#sudo install -o"$(id -u)" -g"$(id -g)" -m400 /keytabs/krb5.keytab /tmp/krb5.keytab
 
-principal="$( klist -kt /tmp/krb5.keytab | grep -Eo -m1 '\w+@[A-Z.]+' )"
-kinit -k -t /tmp/krb5.keytab "$principal"
-klist
+#principal="$( klist -kt /tmp/krb5.keytab | grep -Eo -m1 '\w+@[A-Z.]+' )"
+#kinit -k -t /tmp/krb5.keytab "$principal"
+#klist
 
-cleanup
+#cleanup
 
+echo "start the pod"
