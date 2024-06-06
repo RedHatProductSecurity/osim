b0d028703e6f566f88925bb475f4d16423b97a2e
tboyette <tboyette@redhat.com>
2024-06-06 10:06:37 -0400

    Remove Affect sorting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,7 +10,6 @@
 * Sort Advanced Search Options alphabetically (`OSIDB-2805`)
 * Add tracker links on affects and flaw form (`OSIDB-2630`)
 * Add button to expand all affects (`OSIDB-2817`)
-* Sort affects by product family, alphabetically (`OSIDB-2533`)
 * Sort impact options logically (`OSIDB-2791`)
 * Advanced search on emptiness for CVE ID (`OSIDB-2806`)
 
--- src/components/FlawForm.vue
+++ src/components/FlawForm.vue
@@ -23,8 +23,6 @@ import { useFlawModel } from '@/composables/useFlawModel';
 import { fileTracker, trackerUrl, type TrackersFilePost } from '@/services/TrackerService';
 import { type ZodFlawType, summaryRequiredStates } from '@/types/zodFlaw';
 import { useDraftFlawStore } from '@/stores/DraftFlawStore';
-import { ascend, prop, sortWith } from 'ramda';
-import type { ZodAffectType } from '@/types/zodAffect';
 
 const props = defineProps<{
   flaw: any;
@@ -94,16 +92,6 @@ watch(() => props.flaw, () => { // Shallow watch so as to avoid reseting on any 
 const isEmbargoed = computed(() => initialFlaw?.embargoed);
 const showUnembargoingModal = ref(false);
 const unembargoing = computed(() => isEmbargoed.value && !flaw.value.embargoed);
-
-const theAffects = computed(() => sortWith(
-  [
-    // get product value using Ramda ascend function as comparator
-    ascend((affect: ZodAffectType) => affect.ps_product ?? ''),
-    ascend(prop('ps_module')),
-    ascend(prop('ps_component'))
-  ],
-  flaw.value.affects
-));
 
 const onSubmit = async () => {
   if (props.mode === 'edit') {
@@ -434,7 +422,7 @@ const toggleMitigation = () => {
       </div>
       <AffectedOfferings
         v-if="mode === 'edit'"
-        :theAffects="theAffects"
+        :theAffects="flaw.affects"
         :affectsToDelete="affectsToDelete"
         class="osim-flaw-form-section"
         :error="errors.affects"
