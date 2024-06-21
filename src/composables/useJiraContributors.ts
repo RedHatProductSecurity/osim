211d5302eaa1241fae13a7052e61af525957afdc
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2024-06-21 14:17:55 +0200

    Add contributors field

---
+++ src/composables/useJiraContributors.ts
@@ -0,0 +1,63 @@
+import { ref } from 'vue';
+import type { ZodJiraContributorType } from '@/types/zodJira';
+import { getJiraIssue, putJiraIssue, searchJiraUsers } from '@/services/JiraService';
+import { createCatchHandler } from './service-helpers';
+import { useMemoize, watchIgnorable } from '@vueuse/core';
+
+export default function useJiraContributors(task_key: string) {
+  const contributors = ref<Partial<ZodJiraContributorType>[]>([]);
+  const hasNewContributors = ref(false);
+  const isLoadingContributors = ref(false);
+
+  const { ignoreUpdates } = watchIgnorable(contributors, () => {
+    console.log('Contributors updated');
+    hasNewContributors.value = true;
+  }, { deep: true });
+
+  const loadJiraContributors = async () => {
+    isLoadingContributors.value = true;
+
+    const data = await getJiraIssue(task_key)
+      .catch(createCatchHandler('Failed to load contributors', false));
+
+    ignoreUpdates(() => {
+      contributors.value = data?.data?.fields.customfield_12315950 ?? [];
+    });
+    isLoadingContributors.value = false;
+  };
+
+  const searchContributors = useMemoize(async (query: string) => {
+    if (!query) {
+      return [];
+    }
+    isLoadingContributors.value = true;
+    const users = await searchJiraUsers(query)
+      .catch(createCatchHandler('Failed to search contributors', false));
+    isLoadingContributors.value = false;
+    return users?.data?.users ?? [];
+  });
+
+  const saveContributors = async () => {
+    if (!hasNewContributors.value) {
+      return;
+    }
+
+    return await putJiraIssue(task_key, {
+      data: {
+        fields: {
+          customfield_12315950: contributors.value,
+        }
+      },
+    });
+  };
+
+  return {
+    contributors,
+    hasNewContributors,
+    isLoadingContributors,
+    loadJiraContributors,
+    searchContributors,
+    saveContributors,
+
+  };
+}
