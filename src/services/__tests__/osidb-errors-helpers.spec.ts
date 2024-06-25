ade069f2323bc42dcd17a69c52045bca444b0787
superbuggy <jpqreichard@gmail.com>
2024-06-25 09:50:17 -0400

    ✅ Test case where response is in an HTML format
    Credit to @MrMarble for the test case code

--- src/services/__tests__/osidb-errors-helpers.spec.ts
+++ src/services/__tests__/osidb-errors-helpers.spec.ts
@@ -4,72 +4,86 @@ import {
 } from '../osidb-errors-helpers';
 
 
-describe('getDisplayedOsidbError', () => {
+describe('OSIDB Error helpers', () => {
+  describe('getDisplayedOsidbError', () => {
 
-  it('should return a string with the error message', () => {
-    const error = {
-      response: {
-        status: 500,
-        statusText: 'Internal Server Error',
-        data: 'Internal Server Error',
-      },
-    };
-    expect(getDisplayedOsidbError(error)).toBe(
-      'OSIDB responded with error 500 (Internal Server Error). \nInternal Server Error'
-    );
-  });
-
-  it('should return a string with the error message when error.response.data is an object', () => {
-    const error = {
-      response: {
-        status: 500,
-        statusText: 'Internal Server Error',
-        data: { error: 'Internal Server Error' },
-      },
-    };
-    expect(getDisplayedOsidbError(error)).toBe(
-      'OSIDB responded with error 500 (Internal Server Error). \nerror: Internal Server Error'
-    );
-  });
-
-  it('should return a string with the error message when error.request is an object', () => {
-    const error = {
-      request: { error: 'Internal Server Error' },
-    };
-    const expectJsonStringified = '{\n  "error": "Internal Server Error"\n}';
-    expect(getDisplayedOsidbError(error)).toBe(expectJsonStringified);
-  });
-
-  it('should return a string with the error message when error.request is **NOT** an object', () => {
-    const error = {
-      request: 'Internal Server Error',
-    };
-    expect(getDisplayedOsidbError(error)).toBe('Internal Server Error');
-  });
-});
-
-describe('parseOsidbErrors', () => {
-  it('should return a string with the error message', () => {
-    const errors = [
-      {
+    it('should return a string with the error message', () => {
+      const error = {
         response: {
           status: 500,
           statusText: 'Internal Server Error',
           data: 'Internal Server Error',
         },
-      },
-      {
+      };
+      expect(getDisplayedOsidbError(error)).toBe(
+        'OSIDB responded with error 500 (Internal Server Error). \nInternal Server Error'
+      );
+    });
+
+    it('should return a string with the error message when error.response.data is an object', () => {
+      const error = {
         response: {
           status: 500,
           statusText: 'Internal Server Error',
-          data: 'Internal Server Error',
+          data: { error: 'Internal Server Error' },
         },
-      },
-    ];
-    expect(parseOsidbErrors(errors)).toBe(
-      'OSIDB responded with error 500 (Internal Server Error). \n'
-      + 'Internal Server Error\n\nOSIDB responded with error 500 (Internal Server Error). \n'
-      + 'Internal Server Error'
-    );
+      };
+      expect(getDisplayedOsidbError(error)).toBe(
+        'OSIDB responded with error 500 (Internal Server Error). \nerror: Internal Server Error'
+      );
+    });
+
+    it('should return a string with the error message when error.request is an object', () => {
+      const error = {
+        request: { error: 'Internal Server Error' },
+      };
+      const expectJsonStringified = '{\n  "error": "Internal Server Error"\n}';
+      expect(getDisplayedOsidbError(error)).toBe(expectJsonStringified);
+    });
+
+    it('should return a string with the error message when error.request is **NOT** an object', () => {
+      const error = {
+        request: 'Internal Server Error',
+      };
+      expect(getDisplayedOsidbError(error)).toBe('Internal Server Error');
+    });
+  });
+
+  it('should parse errors from HTML responses', () => {
+    const error = {
+      response: {
+        headers: {
+          'content-type': 'text/html',
+        },
+        data: '<div class="exception_value">Some error on line 1</div>',
+      }
+    };
+    expect(getDisplayedOsidbError(error)).toBe('Likely error between OSIDB and database:\nSome error on line 1');
+  });
+
+  describe('parseOsidbErrors', () => {
+    it('should return a string with each error message separated by a pair of newlines', () => {
+      const errors = [
+        {
+          response: {
+            status: 500,
+            statusText: 'Internal Server Error',
+            data: 'Internal Server Error',
+          },
+        },
+        {
+          response: {
+            status: 500,
+            statusText: 'Internal Server Error',
+            data: 'Internal Server Error',
+          },
+        },
+      ];
+      expect(parseOsidbErrors(errors)).toBe(
+        'OSIDB responded with error 500 (Internal Server Error). \n'
+          + 'Internal Server Error\n\nOSIDB responded with error 500 (Internal Server Error). \n'
+          + 'Internal Server Error'
+      );
+    });
   });
 });
