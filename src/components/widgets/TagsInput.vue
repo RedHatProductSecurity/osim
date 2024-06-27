6af3b2c2d021d8e185a47bfc15bbd511cfa597b8
C-Valen <charlie.valen@gmail.com>
2024-06-27 12:56:55 +0200

    💄 Adjust component input field height

--- src/components/widgets/TagsInput.vue
+++ src/components/widgets/TagsInput.vue
@@ -146,10 +146,12 @@ function edit(index: number, event: Event) {
 
 .osim-pill-list-item {
   font-size: .85rem;
+  padding-block: 0.1rem;
 }
 
 .osim-pill-list-input {
   display: inline-block;
+  height: 1.5rem;
   border: none;
   outline: none;
   box-shadow: none;
