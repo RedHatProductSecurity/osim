290b9797aae108b0b8a0844c2298a93b867f9c6a
superbuggy <jpqreichard@gmail.com>
2024-07-17 14:57:53 -0400

    ✨ Support multiflaw tracking # This is a combination of 10 commits.
    🚧 Implement basic service
    
    🐛 Remove merge artefacts
    
    🐛 Fix check for affect presence
    
    🐛 Fix unmount cleanup function on loading directive
    
    The cause seems to have been 'patch-style' updates to pieces of the DOM that were done via hot-reloading.
    
    ♻️ Ensure type signature
    
    🚧 Request trackers for related flaws
    
    OSIDB endpoint seems to be returning only trackers for affect of current flaw.
    
    ✨ Provide new Tabs component with dynamic slot scopes
    
    🚧 Provide UI for multiflaws
    
    Filing trackers doesn't work due to module_components from OSIDB's tracker querying endpoint only having an association with a single affect.
    
    ♻️ Unpack object error values from OSIDB error response data
    
    🚧 Synchronize tracker selections
    
    💄 Use tracker theme color for trackers manager
    
    💄 Use tracker theme color for trackers manager
    
    💄  Use tracker theme color on related Flaw tabs
    
    💄  Refine trackers manager layout
    
    ✨ Connect filing for multiple flaws
    
    🔥 Remove unused file
    
    🏷️ Improve type safety
    
    💄 Place Filed and Unfiled Trackers side-by-side
    
    💄 Make untrackable affects alert full-screen
    
    🔥 Remove unused functions
    
    💄  Refine trackers manager layout
    
    🏷️ Improve type usage in single flaw trackers manager
    
    ✨ Consolidate multi-flaw requests
    
    ✨ Make multi-flaw trackers filterable
    
    ✨ Add option for bulk filing
    
    ✨ Make related flaws opt-in rather than automatic
    
    🐛 Prevent unintended tracker matches
    
    ✨ Make multi-cve list fitlerable
    
    💄 Enlarge add related flaw button tab
    
    ✨ Fetch flaw manually if not found in related flaws

---
+++ src/components/widgets/TabsDynamic.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { isCveValid } from '@/utils/helpers';
+
+const props = defineProps<{
+  labels: string[];
+  slotProps: Record<string, any>;
+  addableItems?: any[];
+}>();
+
+const emit = defineEmits<{
+  'add-tab': [value?: any];
+}>();
+
+const activeTabIndex = ref(0);
+
+const selectTab = (index: number) => {
+  activeTabIndex.value = index;
+};
+
+const searchFilter = ref('');
+const filteredItems = computed(() => props.addableItems?.filter((item: string) => searchFilter.value
+  ? item.toLowerCase().includes(searchFilter.value.toLowerCase())
+  : item
+));
+
+</script>
+
+<template>
+  <div>
+    <ul class="nav nav-tabs">
+      <li v-for="(label, index) in labels" :key="index" class="nav-item">
+        <button
+          type="button"
+          class="nav-link"
+          :class="{ 'active': activeTabIndex === index }"
+          @click="selectTab(index)"
+        >
+          {{ label }}
+        </button>
+      </li>
+      <li v-if="addableItems?.length" class="nav-item">
+        <button
+          type="button"
+          class="nav-link osim-add-tab"
+          data-bs-toggle="dropdown"
+        >
+          <i class="bi bi-plus"></i>
+        </button>
+        <ul class="osim-dropdown-menu dropdown-menu dropdown-menu-end">
+          <li>
+            <input
+              v-model="searchFilter"
+              class="border border-info px-1 mx-2 focus-ring focus-ring-info"
+              type="text"
+              placeholder="Search..."
+              @submit.prevent="emit('add-tab', searchFilter)"
+            />
+            <button
+              :disabled="!isCveValid(searchFilter)"
+              class="btn btn-info btn-sm"
+              type="button"
+              @click="emit('add-tab', searchFilter)"
+            >Add</button>
+          </li>
+          <li v-for="item in filteredItems" :key="item">
+            <button
+              class="dropdown-item"
+              type="button"
+              @click="emit('add-tab', item)"
+            >
+              {{ item }}
+            </button>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div v-for="(label, index) in labels" :key="index" :class="{ 'visually-hidden': index !== activeTabIndex }">
+      <slot
+        :key="index"
+        :name="label"
+        v-bind="slotProps[label]"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.nav-link:not(:active) {
+  color: gray;
+}
+
+.osim-add-tab {
+  background-color: #fff;
+}
+
+.osim-dropdown-menu {
+  max-height: 12rem;
+  overflow-y: auto;
+}
+</style>
