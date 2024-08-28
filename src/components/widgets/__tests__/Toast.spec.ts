3399cabd3dc13e0fea9ad699d4d64196d663bd62
Alvaro Tinoco <atinocom@redhat.com>
2024-08-28 11:04:46 +0200

    ✅ Refactor tests

--- src/components/widgets/__tests__/Toast.spec.ts
+++ src/components/widgets/__tests__/Toast.spec.ts
@@ -1,172 +1,70 @@
-import { VueWrapper, mount, flushPromises } from '@vue/test-utils';
-import { describe, it, expect } from 'vitest';
-import router from '@/router';
+import { mount, flushPromises } from '@vue/test-utils';
 import { createTestingPinia } from '@pinia/testing';
 
-import Toast from '../Toast.vue';
-import { useSettingsStore } from '@/stores/SettingsStore';
+import Toast, { type ToastProps } from '../Toast.vue';
 import { DateTime } from 'luxon';
-import ProgressRing from '../ProgressRing.vue';
 
-describe('Toast', async () => {
-  let subject: VueWrapper<InstanceType<typeof Toast>>;
+describe('Toast', () => {
+  const mountToast = (props?: Partial<ToastProps>) => {
+    return mount(Toast, {
+      props: {
+        timestamp: DateTime.fromISO('2024-08-28T10:10:10.000Z'),
+        body: 'Test body',
+        ...props,
+      },
+      global: {
+        plugins: [createTestingPinia()]
+      }
+    });
+  };
+
   beforeEach(() => {
-    vi.useFakeTimers();
+    vi.useFakeTimers({
+      now: new Date('2024-08-28T11:10:10.000Z').getTime(),
+    });
   });
 
   afterEach(() => {
     vi.useRealTimers();
-    vi.clearAllMocks();
   });
 
-  it('render toast body, header', async () => {
-    const pinia = createTestingPinia({
-      createSpy: vitest.fn,
-      stubActions: false,
-    });
-    const settingStore = useSettingsStore(pinia);
-    settingStore.$state = {
-      settings: {
-        ...settingStore.$state.settings,
-        showNotifications: true,
-      }
-    };
-    subject = mount(Toast, {
-      global: {
-        plugins: [
-          pinia,
-          router,
-        ]
-      },
-      props: {
-        title: 'Toast',
-        body: 'body',
-        timestamp: DateTime.now(),
-        timeoutMs: 10000,
-      }
-    });
-    const toast = subject.find('.osim-toast');
-    expect(toast.exists()).toBeTruthy();
-    const toastHeader = toast.find('.toast-header');
-    expect(toastHeader.exists()).toBeTruthy();
-    const toastTitle = toastHeader.find('.me-auto');
-    expect(toastTitle.exists()).toBeTruthy();
-    expect(toastTitle.text()).toBe('Toast');
-    const toastBody = toast.find('.toast-body');
-    expect(toastBody.exists()).toBeTruthy();
-    const progressRingElement = subject.findAllComponents(ProgressRing);
-    expect(progressRingElement.length).toBe(1);
+
+  it.each<{ title: string, body: string, css: ToastProps['css'] }>([
+    { title: 'Success', body: 'Test body', css: 'success' },
+    { title: 'Error', body: 'Test body', css: 'danger' },
+    { title: 'Warn', body: 'Test body', css: 'warning' },
+    { title: 'Default', body: 'Test body', css: undefined },
+  ])('Should render component with title $title, body $body and $css style', ({ title, body, css }) => {
+    const wrapper = mountToast({ title, body, css });
+
+    expect(wrapper.html()).toMatchSnapshot();
   });
 
-  it('trigger close event', async () => {
-    const pinia = createTestingPinia({
-      createSpy: vitest.fn,
-      stubActions: false,
-    });
-    const settingStore = useSettingsStore(pinia);
-    settingStore.$state = {
-      settings: {
-        ...settingStore.$state.settings,
-        showNotifications: true,
-      }
-    };
-    subject = mount(Toast, {
-      global: {
-        plugins: [
-          pinia,
-          router,
-        ]
-      },
-      props: {
-        title: 'Toast',
-        body: 'body',
-        timestamp: DateTime.now(),
-        timeoutMs: 10000,
-        onClose: vitest.fn,
-      }
-    });
-    const toast = subject.find('.osim-toast');
-    expect(toast.exists()).toBeTruthy();
-    const closeButton = toast.find('.btn-close');
-    expect(closeButton.exists()).toBeTruthy();
-    await closeButton.trigger('click');
-    expect(subject.emitted('close')).toBeTruthy();
+  it('Should close toast when close button is clicked', async () => {
+    const wrapper = mountToast();
+
+    await wrapper.find('.btn-close').trigger('click');
+
+    expect(wrapper.emitted('close')).toBeTruthy();
   });
 
-  it('trigger close event after timeoutMs', async () => {
-    const pinia = createTestingPinia({
-      createSpy: vitest.fn,
-      stubActions: false,
-    });
-    const settingStore = useSettingsStore(pinia);
-    settingStore.$state = {
-      settings: {
-        ...settingStore.$state.settings,
-        showNotifications: true,
-      }
-    };
-    subject = mount(Toast, {
-      global: {
-        plugins: [
-          pinia,
-          router,
-        ]
-      },
-      props: {
-        title: 'Toast',
-        body: 'body',
-        timestamp: DateTime.now(),
-        timeoutMs: 1000,
-        onClose: vitest.fn,
-      }
-    });
-    const toast = subject.find('.osim-toast');
-    expect(toast.exists()).toBeTruthy();
-    const closeButton = toast.find('.btn-close');
-    expect(closeButton.exists()).toBeTruthy();
-    vi.advanceTimersByTime(7000);
+  it('Should close toast after timeoutMs', async () => {
+    const wrapper = mountToast({ timeoutMs: 500 });
+
+    vi.advanceTimersByTime(1000);
     await flushPromises();
-    expect(subject.emitted('close')).toBeTruthy();
+
+    expect(wrapper.emitted('close')).toBeTruthy();
+    expect(wrapper.emitted('stale')).toBeFalsy();
   });
 
-  it('trigger stale event after freshMs', async () => {
-    const pinia = createTestingPinia({
-      createSpy: vitest.fn,
-      stubActions: false,
-    });
-    const settingStore = useSettingsStore(pinia);
-    settingStore.$state = {
-      settings: {
-        ...settingStore.$state.settings,
-        showNotifications: true,
-      }
-    };
-    subject = mount(Toast, {
-      global: {
-        plugins: [
-          pinia,
-          router,
-        ]
-      },
-      props: {
-        title: 'Toast',
-        body: 'body',
-        timestamp: DateTime.now(),
-        timeoutMs: 10000,
-        onClose: vitest.fn,
-        onStale: vitest.fn
-      }
-    });
-    const toast = subject.find('.osim-toast');
-    expect(toast.exists()).toBeTruthy();
-    const closeButton = toast.find('.btn-close');
-    expect(closeButton.exists()).toBeTruthy();
+  it('Should emit stale event after freshMs', async () => {
+    const wrapper = mountToast({ timeoutMs: 50000 });
+
     vi.advanceTimersByTime(11000);
     await flushPromises();
-    expect(subject.emitted('stale')).toBeTruthy();
-    vi.advanceTimersByTime(5000);
-    await flushPromises();
-    expect(subject.emitted('close')).toBeTruthy();
+
+    expect(wrapper.emitted('close')).toBeFalsy();
+    expect(wrapper.emitted('stale')).toBeTruthy();
   });
 });
-
