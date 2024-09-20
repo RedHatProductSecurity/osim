aa8cd09e8d8443d3ad89fd10a7db8f36bd5e3c11
C-Valen <charlie.valen@gmail.com>
2024-09-20 11:23:28 +0200

    🔥 Remove irelevant sorting fields

--- src/components/IssueSearchAdvanced.vue
+++ src/components/IssueSearchAdvanced.vue
@@ -64,12 +64,10 @@ const nameForOption = (fieldName: string) => {
 };
 
 const customSortingFields = [
-  'cve_description',
   'cvss_scores__score',
   'cwe_id',
   'major_incident_state',
   'source',
-  'statement',
 ];
 
 const fieldNamesSorted = sort((fieldA: string, fieldB: string) =>
