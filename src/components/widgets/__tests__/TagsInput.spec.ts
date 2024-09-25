fcf80178f9a5d075b843f504be2845bbc4a2b1a4
Alvaro Tinoco <atinocom@redhat.com>
2024-09-25 13:22:25 +0200

    ✅ Increase code coverage for widgets

--- src/components/widgets/__tests__/TagsInput.spec.ts
+++ src/components/widgets/__tests__/TagsInput.spec.ts
@@ -1,28 +1,60 @@
 import { mount } from '@vue/test-utils';
 
-import TagsInput from '../TagsInput.vue';
+import TagsInput from '@/components/widgets/TagsInput.vue';
 
 describe('tagsInput', () => {
-  it('renders properly', () => {
-    const subject = mount(TagsInput, {
-      props: {
-        modelValue: ['test1', 'test2'],
-        error: '',
-      },
-    });
-
-    expect(subject.text()).toContain('test1');
-    expect(subject.text()).toContain('test2');
-  });
-
-  it('renders properly with error', () => {
-    const subject = mount(TagsInput, {
+  it('renders correctly with default props', () => {
+    const wrapper = mount(TagsInput, {
       props: {
         modelValue: [],
-        error: 'some error',
       },
     });
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.findAll('.osim-pill-list-item').length).toBe(0);
+  });
 
-    expect(subject.text()).toContain('error');
+  it('adds new items correctly', async () => {
+    const wrapper = mount(TagsInput, {
+      props: {
+        modelValue: [],
+      },
+    });
+    const input = wrapper.find('input');
+    await input.setValue('New Item');
+    await input.trigger('keydown.enter');
+    expect(wrapper.props('modelValue')).toContain('New Item');
+  });
+
+  it('removes items correctly', async () => {
+    const wrapper = mount(TagsInput, {
+      props: {
+        modelValue: ['Item 1', 'Item 2'],
+      },
+    });
+    const removeButton = wrapper.findAll('.bi-x-square').at(0);
+    await removeButton!.trigger('click');
+    expect(wrapper.props('modelValue')).not.toContain('Item 1');
+  });
+
+  it('edits items correctly', async () => {
+    const wrapper = mount(TagsInput, {
+      props: {
+        modelValue: ['Item 1'],
+      },
+    });
+    await wrapper.find('.osim-pill-list-item span').trigger('click');
+    await wrapper.find('input').setValue('Edited Item');
+    await wrapper.find('input').trigger('keydown.enter');
+    expect(wrapper.props('modelValue')).toContain('Edited Item');
+  });
+
+  it('displays error message correctly', () => {
+    const wrapper = mount(TagsInput, {
+      props: {
+        modelValue: [],
+        error: 'Test Error',
+      },
+    });
+    expect(wrapper.find('.invalid-tooltip').text()).toBe('Test Error');
   });
 });
