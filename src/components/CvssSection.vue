6e8fc5c0d27f519d026a744162c774c1538a91eb
C-Valen <charlie.valen@gmail.com>
2024-10-16 14:19:06 +0200

    ✨ Add cvss section

---
+++ src/components/CvssSection.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import CvssNISTForm from '@/components/CvssNISTForm.vue';
+import LabelDiv from '@/components/widgets/LabelDiv.vue';
+
+import type { ZodFlawCVSSType } from '@/types/zodFlaw';
+
+const props = defineProps<{
+  allScores: ZodFlawCVSSType[];
+  bugzilla: string;
+  cveid: null | string;
+  cvss: string;
+  highlightedNvdCvss3String: { char: null | string; isHighlighted: boolean }[][];
+  nistcvss: string;
+  shouldDisplayEmailNistForm: boolean;
+  summary: string;
+}>();
+
+const otherScores = computed(() => props.allScores.filter(score =>
+  (!(score.cvss_version === 'V3' && (score.issuer === 'RH' || score.issuer === 'NIST')))));
+</script>
+
+<template>
+  <div>
+    <LabelDiv label="NVD CVSSv3">
+      <div class="d-flex flex-row">
+        <div class="form-control text-break h-auto rounded-0">
+          <template v-for="(chars, index) in highlightedNvdCvss3String" :key="index">
+            <span v-if="chars[0].isHighlighted" class="text-primary">
+              {{ chars.map(c => c.char).join('') }}
+            </span>
+            <template v-else>{{ chars.map(c => c.char).join('') }}</template>
+          </template>
+        </div>
+        <div v-if="shouldDisplayEmailNistForm" class="col-auto align-self-center">
+          <CvssNISTForm
+            :cveid
+            :summary
+            :bugzilla
+            :cvss
+            :nistcvss
+          />
+        </div>
+      </div>
+    </LabelDiv>
+    <template v-for="score in otherScores" :key="score.uuid">
+      <LabelDiv :label="score.issuer + ' ' + score.cvss_version">
+        <div class="d-flex flex-row">
+          <div class="form-control text-break h-auto">
+            <template v-for="(chars, index) in highlightedNvdCvss3String" :key="index">
+              <span v-if="chars[0].isHighlighted" class="text-primary">
+                {{ chars.map(c => c.char).join('') }}
+              </span>
+              <template v-else>{{ chars.map(c => c.char).join('') }}</template>
+            </template>
+          </div>
+        </div>
+      </LabelDiv>
+    </template>
+  </div>
+</template>
