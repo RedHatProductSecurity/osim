4ef53442c633c4209629d329fd8d899d0f24a38b
superbuggy <jpqreichard@gmail.com>
2024-10-21 14:36:41 -0400

    ✅ Restore fidelity to testing data for trackers

--- src/composables/__tests__/useRelatedFlawTrackers.spec.ts
+++ src/composables/__tests__/useRelatedFlawTrackers.spec.ts
@@ -2,8 +2,6 @@ import { ref, type Ref } from 'vue';
 
 import { flushPromises } from '@vue/test-utils';
 
-// import { affects } from '@/components/__tests__/__fixtures__/sampleFlawFull.json';
-
 import sampleTrackersQueryResult from '@/components/__tests__/__fixtures__/sampleTrackersQueryResult.json';
 
 import { useRelatedFlawTrackers } from '@/composables/useRelatedFlawTrackers';
@@ -11,7 +9,7 @@ import { useRelatedFlawTrackers } from '@/composables/useRelatedFlawTrackers';
 import { fileTrackingFor, getTrackersForFlaws } from '@/services/TrackerService';
 import type { ZodFlawType } from '@/types';
 
-import * as flawJson from '../../components/__tests__/__fixtures__/sampleFlawFull.json';
+import * as flawJson from '../../components/__tests__/__fixtures__/sampleFlawTrackers.json';
 
 const flaw = flawJson as ZodFlawType;
 
@@ -85,22 +83,20 @@ describe('useRelatedFlawTrackers', () => {
     const { multiFlawTrackers } = await mockedTrackers(flaw, relatedFlaws);
     const untrackedAffects = Object.values(
       multiFlawTrackers.value).flatMap(({ untrackedAffects }) => untrackedAffects);
-    expect(untrackedAffects.length).toBe(4);
+    expect(untrackedAffects.length).toBe(20);
   });
 
   it('computes untrackableAffects correctly', async () => {
-    const sampleTrackers = { modules_components: [], not_applicable: [] };
-    const { multiFlawTrackers } = await mockedTrackers(flaw, relatedFlaws, sampleTrackers);
+    const { multiFlawTrackers } = await mockedTrackers(flaw, relatedFlaws);
     const untrackableAffects = Object.values(
       multiFlawTrackers.value).flatMap(({ untrackableAffects }) => untrackableAffects);
-    expect(untrackableAffects.length).toBe(1);
+    expect(untrackableAffects.length).toBe(16);
   });
 
   it('computes availableUpdateStreams correctly', async () => {
     const trackers = await mockedTrackers(flaw, relatedFlaws);
     const availableUpdateStreams = Object.values(trackers.multiFlawTrackers.value).flatMap(
       ({ availableUpdateStreams }) => availableUpdateStreams);
-    console.log(availableUpdateStreams);
     expect(availableUpdateStreams).toEqual(mockedOsimUpdateStreams());
   });
 });
@@ -110,122 +106,134 @@ function mockedOsimUpdateStreams() {
     {
       ps_update_stream: 'rhel-br-8.10.z',
       selected: false,
-      acked: false,
+      acked: true,
       eus: false,
       aus: false,
       ps_component: 'javapackages-tools:201801/junit',
       ps_module: 'rhel-br-8',
       affectUuid: '1540c450-4422-49b9-bc3d-1c16bfe379b8',
+      flawCveOrId: 'CVE-2020-15250',
     },
     {
       ps_update_stream: 'rhel-br-8.6.0.z',
       selected: false,
-      acked: false,
-      eus: false,
+      acked: true,
+      eus: true,
       aus: false,
       ps_component: 'javapackages-tools:201801/junit',
       ps_module: 'rhel-br-8',
       affectUuid: '1540c450-4422-49b9-bc3d-1c16bfe379b8',
+      flawCveOrId: 'CVE-2020-15250',
     },
     {
       ps_update_stream: 'rhel-br-8.8.0.z',
       selected: false,
-      acked: false,
-      eus: false,
+      acked: true,
+      eus: true,
       aus: false,
       ps_component: 'javapackages-tools:201801/junit',
       ps_module: 'rhel-br-8',
       affectUuid: '1540c450-4422-49b9-bc3d-1c16bfe379b8',
-    },
-    {
-      ps_update_stream: 'rhel-br-8.4.0.z',
-      selected: false,
-      acked: false,
-      eus: false,
-      aus: false,
-      ps_component: 'javapackages-tools:201801/junit',
-      ps_module: 'rhel-br-8',
-      affectUuid: '1540c450-4422-49b9-bc3d-1c16bfe379b8',
+      flawCveOrId: 'CVE-2020-15250',
     },
     {
       ps_update_stream: 'rhel-br-8.2.0.z',
       selected: false,
-      acked: false,
+      acked: true,
       eus: false,
-      aus: false,
+      aus: true,
       ps_component: 'javapackages-tools:201801/junit',
       ps_module: 'rhel-br-8',
       affectUuid: '1540c450-4422-49b9-bc3d-1c16bfe379b8',
+      flawCveOrId: 'CVE-2020-15250',
+    },
+    {
+      ps_update_stream: 'rhel-br-8.4.0.z',
+      selected: false,
+      acked: true,
+      eus: false,
+      aus: true,
+      ps_component: 'javapackages-tools:201801/junit',
+      ps_module: 'rhel-br-8',
+      affectUuid: '1540c450-4422-49b9-bc3d-1c16bfe379b8',
+      flawCveOrId: 'CVE-2020-15250',
     },
     {
       ps_update_stream: 'rhel-br-8',
-      selected: true,
+      selected: false,
       acked: false,
       eus: false,
       aus: false,
       ps_component: 'javapackages-tools:201801/junit',
       ps_module: 'rhel-br-8',
       affectUuid: '1540c450-4422-49b9-bc3d-1c16bfe379b8',
-    },
-    {
-      ps_update_stream: 'rhel-8.10.z',
-      selected: false,
-      acked: false,
-      eus: false,
-      aus: false,
-      ps_component: 'eclipse:rhel8/junit',
-      ps_module: 'rhel-8',
-      affectUuid: 'a45e50c3-6d3f-484d-b8a3-ffc1971a1d2f',
-    },
-    {
-      ps_update_stream: 'rhel-8.6.0.z',
-      selected: false,
-      acked: false,
-      eus: false,
-      aus: false,
-      ps_component: 'eclipse:rhel8/junit',
-      ps_module: 'rhel-8',
-      affectUuid: 'a45e50c3-6d3f-484d-b8a3-ffc1971a1d2f',
+      flawCveOrId: 'CVE-2020-15250',
     },
     {
       ps_update_stream: 'rhel-8.8.0.z',
       selected: false,
-      acked: false,
-      eus: false,
+      acked: true,
+      eus: true,
       aus: false,
       ps_component: 'eclipse:rhel8/junit',
       ps_module: 'rhel-8',
       affectUuid: 'a45e50c3-6d3f-484d-b8a3-ffc1971a1d2f',
+      flawCveOrId: 'CVE-2020-15250',
     },
     {
-      ps_update_stream: 'rhel-8.2.0.z',
+      ps_update_stream: 'rhel-8.6.0.z',
       selected: false,
-      acked: false,
-      eus: false,
+      acked: true,
+      eus: true,
       aus: false,
       ps_component: 'eclipse:rhel8/junit',
       ps_module: 'rhel-8',
       affectUuid: 'a45e50c3-6d3f-484d-b8a3-ffc1971a1d2f',
+      flawCveOrId: 'CVE-2020-15250',
     },
     {
       ps_update_stream: 'rhel-8.4.0.z',
       selected: false,
-      acked: false,
+      acked: true,
+      eus: false,
+      aus: true,
+      ps_component: 'eclipse:rhel8/junit',
+      ps_module: 'rhel-8',
+      affectUuid: 'a45e50c3-6d3f-484d-b8a3-ffc1971a1d2f',
+      flawCveOrId: 'CVE-2020-15250',
+    },
+    {
+      ps_update_stream: 'rhel-8.2.0.z',
+      selected: false,
+      acked: true,
+      eus: false,
+      aus: true,
+      ps_component: 'eclipse:rhel8/junit',
+      ps_module: 'rhel-8',
+      affectUuid: 'a45e50c3-6d3f-484d-b8a3-ffc1971a1d2f',
+      flawCveOrId: 'CVE-2020-15250',
+    },
+    {
+      ps_update_stream: 'rhel-8.10.z',
+      selected: false,
+      acked: true,
       eus: false,
       aus: false,
       ps_component: 'eclipse:rhel8/junit',
       ps_module: 'rhel-8',
       affectUuid: 'a45e50c3-6d3f-484d-b8a3-ffc1971a1d2f',
+      flawCveOrId: 'CVE-2020-15250',
     },
     {
-      ps_update_stream: 'fuse-7',
-      selected: true,
-      acked: false,
+      ps_update_stream: 'fuse-7-els-1',
+      selected: false,
+      acked: true,
       eus: false,
       aus: false,
       ps_component: 'junit4',
       ps_module: 'fuse-7',
       affectUuid: 'a5cfd8ff-b12d-4408-9309-607785551faf',
+      flawCveOrId: 'CVE-2020-15250',
     },
   ];
 }
