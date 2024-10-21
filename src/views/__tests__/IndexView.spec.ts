95334376e50f5f3503e4b23dc6101117b9d719e0
C-Valen <charlie.valen@gmail.com>
2024-10-21 12:05:15 +0200

    ✅ Update test cases
    ✅ Increase coverage
    
    ✅ Update mocks
    
    ✅ Update snapshots
    
    ✅ Update test cases

--- src/views/__tests__/IndexView.spec.ts
+++ src/views/__tests__/IndexView.spec.ts
@@ -24,8 +24,9 @@ vi.mock('@/composables/useFlawsFetching', () => ({
 
 vi.mock('@/stores/SearchStore', () => ({
   useSearchStore: vi.fn().mockReturnValue({
-    queryFilter: 'django query',
-    searchFilters: { affects__ps_component: 'test' },
+    savedSearches: [
+      { name: 'name', searchFilters: { affects__ps_component: 'test' }, queryFilter: 'django query', isDefault: false },
+    ],
   }),
 }));
 
@@ -51,16 +52,13 @@ describe('indexView', () => {
     expect(loadFlaws).toHaveBeenCalledWith(expect.objectContaining({
       _value: {
         order: '-created_dt',
-        affects__ps_component: 'test',
-        query: 'django query',
       },
     }));
   });
 
   it('should call loadFlaws on mount without filters', async () => {
     vi.mocked(useSearchStore, { partial: true }).mockReturnValue({
-      queryFilter: '',
-      searchFilters: {},
+      savedSearches: [],
     });
     const { loadFlaws } = useFlawsFetching();
     const wrapper = mountWithConfig(IndexView);
@@ -74,6 +72,45 @@ describe('indexView', () => {
         order: '-created_dt',
       },
     }));
+  });
+
+  it('should load default saved search when set', async () => {
+    vi.mocked(useSearchStore, { partial: true }).mockReturnValue({
+      savedSearches: [
+        {
+          name: 'name',
+          searchFilters: { affects__ps_component: 'test' },
+          queryFilter: 'django query',
+          isDefault: true,
+        },
+      ],
+      defaultSearch: null,
+    });
+    const wrapper = mountWithConfig(IndexView);
+
+    await flushPromises();
+
+    const savedSearches = wrapper.findAll('details > div > .btn');
+    expect(savedSearches[0].find('i.bi-star-fill').exists()).toBeTruthy();
+  });
+
+  it('should not load default saved search when not set', async () => {
+    vi.mocked(useSearchStore, { partial: true }).mockReturnValue({
+      savedSearches: [
+        {
+          name: 'name',
+          searchFilters: { affects__ps_component: 'test' },
+          queryFilter: 'django query',
+          isDefault: false,
+        },
+      ],
+    });
+    const wrapper = mountWithConfig(IndexView);
+
+    await flushPromises();
+
+    const savedSearches = wrapper.findAll('details > div > .btn');
+    expect(savedSearches[0].find('i.bi-star-fill').exists()).toBeFalsy();
   });
 
   it('should call loadFlaws when filters are changed', async () => {
@@ -90,6 +127,102 @@ describe('indexView', () => {
     expect(loadFlaws).toHaveBeenCalledWith(expect.objectContaining({
       _value: {
         order: 'updated_dt',
+      },
+    }));
+  });
+
+  it('shouldn\'t render empty saved searches', async () => {
+    vi.mocked(useSearchStore, { partial: true }).mockReturnValue({
+      savedSearches: [],
+    });
+    const wrapper = mountWithConfig(IndexView);
+    const savedSearches = wrapper.findAll('details > div > .btn');
+    expect(savedSearches.length).toBe(0);
+  });
+
+  it('should render saved searches', async () => {
+    vi.mocked(useSearchStore, { partial: true }).mockReturnValue({
+      savedSearches: [
+        {
+          name: 'name',
+          searchFilters: { affects__ps_component: 'test' },
+          queryFilter: 'django query',
+          isDefault: false,
+        },
+      ],
+    });
+    const wrapper = mountWithConfig(IndexView);
+    const savedSearches = wrapper.findAll('details > div > .btn');
+    expect(savedSearches[0].attributes('title')).toBe('Query: django query\nFields: affects__ps_component: test');
+  });
+
+  it('should select saved searches', async () => {
+    vi.mocked(useSearchStore, { partial: true }).mockReturnValue({
+      savedSearches: [
+        {
+          name: 'name',
+          searchFilters: { affects__ps_component: 'test' },
+          queryFilter: 'django query',
+          isDefault: false,
+        },
+      ],
+    });
+    const wrapper: VueWrapper<Partial<{ selectSavedSearch: (index: number) => void }>> = mountWithConfig(IndexView);
+
+    wrapper.vm.selectSavedSearch!(0);
+    await flushPromises();
+
+    const selectedSavedSearch = wrapper.find('details > div > .btn-secondary');
+    expect(selectedSavedSearch.exists()).toBeTruthy();
+  });
+
+  it('should deselect saved searches', async () => {
+    vi.mocked(useSearchStore, { partial: true }).mockReturnValue({
+      savedSearches: [
+        {
+          name: 'name',
+          searchFilters: { affects__ps_component: 'test' },
+          queryFilter: 'django query',
+          isDefault: false,
+        },
+      ],
+    });
+    const wrapper: VueWrapper<Partial<{ selectSavedSearch: (index: number) => void }>> = mountWithConfig(IndexView);
+
+    wrapper.vm.selectSavedSearch!(0);
+    await flushPromises();
+
+    let selectedSavedSearch = wrapper.find('details > div > .btn-secondary');
+    await selectedSavedSearch.trigger('click');
+    selectedSavedSearch = wrapper.find('details > div > .btn-secondary');
+    expect(selectedSavedSearch.exists()).toBeFalsy();
+  });
+
+  it('should apply saved search', async () => {
+    vi.mocked(useSearchStore, { partial: true }).mockReturnValue({
+      savedSearches: [
+        {
+          name: 'name',
+          searchFilters: { affects__ps_component: 'test' },
+          queryFilter: 'django query',
+          isDefault: false,
+        },
+      ],
+    });
+    const { loadFlaws } = useFlawsFetching();
+    const wrapper = mountWithConfig(IndexView);
+
+    const savedSearches = wrapper.findAll('details > div > .btn');
+
+    await savedSearches[0].trigger('click');
+    await flushPromises();
+
+    expect(loadFlaws).toHaveBeenCalledTimes(1);
+    expect(loadFlaws).toHaveBeenCalledWith(expect.objectContaining({
+      _value: {
+        order: '-created_dt',
+        affects__ps_component: 'test',
+        query: 'django query',
       },
     }));
   });
