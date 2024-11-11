46ec65f78d6ed241647f26a9b21803250707ea99
Alvaro Tinoco <atinocom@redhat.com>
2024-11-11 11:15:06 +0100

    🚚 Move fixtures to top level

--- src/components/__tests__/test-suite-helpers.ts
+++ src/components/__tests__/test-suite-helpers.ts
@@ -1,12 +1,12 @@
 import { test } from 'vitest';
 
+import sampleFlawRequired from '@/__tests__/__fixtures__/sampleFlawRequired.json';
 import { IssuerEnum } from '@/generated-client';
 import { CVSS_V3 } from '@/constants';
 import type { ZodFlawType, ZodAffectType } from '@/types';
+import sampleFlawFull from '@/__tests__/__fixtures__/sampleFlawFull.json';
 
 import sampleFlawEmpty from './__fixtures__/sampleFlawEmpty.json';
-import sampleFlawRequired from './__fixtures__/sampleFlawRequired.json';
-import sampleFlawFull from './__fixtures__/sampleFlawFull.json';
 
 let flaw: null | ZodFlawType = null;
 export const osimEmptyFlawTest = test.extend<{ flaw: ZodFlawType }>({
