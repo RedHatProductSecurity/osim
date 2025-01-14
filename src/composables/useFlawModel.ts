441d6bcdf825bf3938a16a7a2c39dc18af6e80a9
superbuggy <jpqreichard@gmail.com>
2025-01-14 10:18:18 -0500

    🐛 Separate refresh logic to avoid weird bundled code in vitest

--- src/composables/useFlawModel.ts
+++ src/composables/useFlawModel.ts
@@ -13,7 +13,6 @@ import { useNetworkQueue } from '@/composables/useNetworkQueue';
 import { validateCvssVector } from '@/composables/useCvssCalculator';
 
 import {
-  getFlaw,
   getFlawBugzillaLink,
   getFlawOsimLink,
   postFlaw,
@@ -230,24 +229,3 @@ function flawErrors(flaw: ZodFlawType) {
   return mirroredFlaw;
 }
 
-export function useRefreshTrackers() {
-  const flaw = useFlaw();
-  const isRefreshingTrackers = ref(false);
-  async function refreshTrackers() {
-    try {
-      isRefreshingTrackers.value = true;
-      const freshFlaw = await getFlaw(flaw.value.uuid);
-      if (freshFlaw) {
-        flaw.value.affects.forEach((affect) => {
-          const freshAffect = freshFlaw.affects.find(freshAffect => freshAffect.uuid === affect.uuid);
-          if (freshAffect) {
-            affect.trackers = freshAffect.trackers;
-          }
-        });
-      }
-    } finally {
-      isRefreshingTrackers.value = false;
-    }
-  }
-  return { isRefreshingTrackers, refreshTrackers };
-}
