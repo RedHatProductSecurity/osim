0418eb5b432cb5ef121d14fbb28a4d4e33c85f58
C-Valen <charlie.valen@gmail.com>
2025-01-14 12:30:22 +0100

    ✅ Update tests

---
+++ src/components/__tests__/CweSelector.spec.ts
@@ -0,0 +1,52 @@
+import { describe, it, beforeEach } from 'vitest';
+
+import CweSelector from '@/components/CweSelector/CweSelector.vue';
+
+import EditableTextWithSuggestions from '@/widgets/EditableTextWithSuggestions/EditableTextWithSuggestions.vue';
+import LabelDiv from '@/widgets/LabelDiv/LabelDiv.vue';
+import { mountWithConfig } from '@/__tests__/helpers';
+
+describe('cweSelector.vue', () => {
+  let wrapper: any;
+
+  beforeEach(() => {
+    wrapper = mountWithConfig(CweSelector, {
+      global: {
+        components: {
+          EditableTextWithSuggestions,
+          LabelDiv,
+        },
+      },
+    });
+  });
+
+  it('renders correctly', () => {
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('loads data from localStorage', () => {
+    const data = JSON.stringify([{ id: '123', name: 'Test CWE', status: 'Draft', summary: '', usage: '' }]);
+    localStorage.setItem('CWE:API-DATA', data);
+    wrapper.vm.loadCweData();
+    expect(wrapper.vm.cweData).toEqual([{ id: '123', name: 'Test CWE', status: 'Draft', summary: '', usage: '' }]);
+  });
+
+  it('filters suggestions correctly', async () => {
+    wrapper.vm.cweData = [
+      { id: '123', name: 'Test CWE', status: 'Draft', summary: '', usage: '' },
+      { id: '456', name: 'Another CWE', status: 'Draft', summary: '', usage: '' },
+    ];
+    await wrapper.vm.filterSuggestions('123');
+    expect(wrapper.vm.suggestions).toEqual([{ id: '123', name: 'Test CWE', status: 'Draft', summary: '', usage: '' }]);
+  });
+
+  it('updates model value correctly on suggestion click', async () => {
+    const fn = vi.fn();
+    wrapper.vm.queryRef = 'CWE-';
+    await wrapper.vm.handleSuggestionClick(fn, 'CWE-123');
+    expect(wrapper.vm.modelValue).toBe('CWE-123');
+    expect(wrapper.vm.queryRef).toBe('CWE-123');
+    expect(wrapper.vm.suggestions).toEqual([]);
+  });
+});
