5084ed5135b549062182a884c2de25364911cbf7
Alvaro Tinoco <atinocom@redhat.com>
2025-01-17 10:41:26 +0100

    🏷️ Update some weak types

--- src/composables/useFlawCvssScores.ts
+++ src/composables/useFlawCvssScores.ts
@@ -7,6 +7,7 @@ import type { ZodFlawType } from '@/types/zodFlaw';
 import { deepCopyFromRaw } from '@/utils/helpers';
 import { IssuerEnum } from '@/generated-client';
 import { CVSS_V3 } from '@/constants';
+import type { Nullable } from '@/utils/typeHelpers';
 
 export const issuerLabels: Record<string, string> = {
   [IssuerEnum.Nist]: 'NVD',
@@ -15,7 +16,7 @@ export const issuerLabels: Record<string, string> = {
   [IssuerEnum.Osv]: 'OSV',
 };
 
-const formatScore = (score: any) => score?.toFixed(1);
+const formatScore = (score: Nullable<number>) => score?.toFixed(1) ?? '';
 // TODO: This composable should be ideally refactored into a more modular
 // solution when CVSSv4 starts being used
 export function useFlawCvssScores(flaw: Ref<ZodFlawType>) {
@@ -123,7 +124,7 @@ export function useFlawCvssScores(flaw: Ref<ZodFlawType>) {
       vector: flawRhCvss3.value?.vector,
       embargoed: flaw.value.embargoed,
     };
-    return postFlawCvssScores(flaw.value.uuid, requestBody as unknown);
+    return postFlawCvssScores(flaw.value.uuid, requestBody);
   }
 
   return {
