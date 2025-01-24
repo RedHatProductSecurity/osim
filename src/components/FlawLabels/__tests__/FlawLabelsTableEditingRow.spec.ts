d5b2050516485be005944e39337b47fa2bc293e0
Alvaro Tinoco <atinocom@redhat.com>
2025-01-24 16:23:20 +0100

    ✨ Create FlawLabels component

---
+++ src/components/FlawLabels/__tests__/FlawLabelsTableEditingRow.spec.ts
@@ -0,0 +1,34 @@
+import { mountWithConfig } from '@/__tests__/helpers';
+
+import FlawLabelTableEditingRow from '../FlawLabelTableEditingRow.vue';
+
+describe('flawLabelsTableEditingRow', () => {
+  it('should render', () => {
+    const wrapper = mountWithConfig(FlawLabelTableEditingRow, { shallow: true });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should not emit save event if nothing changed', async () => {
+    const wrapper = mountWithConfig(FlawLabelTableEditingRow);
+
+    await wrapper.find('button[title="Save"]').trigger('click');
+
+    expect(wrapper.emitted()).not.toHaveProperty('save');
+    expect(wrapper.emitted()).toHaveProperty('cancel');
+  });
+
+  it('should emit save event if value changed', async () => {
+    const wrapper = mountWithConfig(FlawLabelTableEditingRow, {
+      props: {
+        contextLabels: ['test'],
+      },
+    });
+
+    await wrapper.findAll('select')[1].setValue('test');
+    await wrapper.find('button[title="Save"]').trigger('click');
+
+    expect(wrapper.emitted()).toHaveProperty('save');
+    expect(wrapper.emitted('save')).toEqual([[expect.objectContaining({ label: 'test' })]]);
+  });
+});
