59e34160bf80ddd04da451a2f80fa589ed337007
superbuggy <jpqreichard@gmail.com>
2025-03-06 10:14:30 -0500

    ✨ Provide basic CVSS4 support
    ♻️ Convert CVSS4 logic to typescript
    
    🏷️ Refine types
    
    🚧 Add dummy component
    
    🚧 Update dummy component
    
    🐛 Fix score calculation bug
    
    🚚 Improve organization and naming consistency
    
    👔 Support CVSS4 as default
    
    🚚 Move UI mappings in UI constants
    
    🚧 WIP Be agnostic about CVSS version
    
    🔧 Define CVSS 4 UI constants
    
    ✨ Support CVSS v4.0 Calculator
    
    ✅ Update tests and snapshots

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,8 @@
 export const SYSTEM_EMAIL = 'bugzilla@redhat.com';
 
 export const CVSS_V3 = 'V3';
-export const DEFAULT_CVSS_VERSION = CVSS_V3;
+export const CVSS_V4 = 'V4';
+export const DEFAULT_CVSS_VERSION = CVSS_V4;
 
 export const allowedSources = [
   '',
