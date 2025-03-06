18555028ff0323c223b5f7107f555bedd84d0888
superbuggy <jpqreichard@gmail.com>
2025-03-06 10:14:30 -0500

    ✨ Provide basic CVSS4 support
    ♻️ Convert CVSS4 logic to typescript
    
    🏷️ Refine types
    
    🚧 Add dummy component
    
    🚧 Update dummy component
    
    🐛 Fix score calculation bug
    
    🚚 Improve organization and naming consistency
    
    👔 Support CVSS4 as default
    
    🚚 Move UI mappings in UI constants
    
    🚧 WIP Be agnostic about CVSS version
    
    🔧 Define CVSS 4 UI constants
    
    ✨ Support CVSS v4.0 Calculator
    
    ✅ Update tests and snapshots

---
+++ src/components/CvssCalculator/Cvss4Calculator/Cvss4Calculator.vue
@@ -0,0 +1,155 @@
+<script setup lang="ts">
+import { CVSS4MetricsForUI } from '@/components/CvssCalculator/Cvss4Calculator/cvss4-ui-contants';
+
+import { useCvss4Selections, useCvss4Calculations } from '@/composables/useCvss4Calculator';
+
+import Modal from '@/widgets/Modal/Modal.vue';
+import { MetricNamesWithValues } from '@/utils/cvss40';
+
+defineProps<{
+  highlightedFactor: null | string;
+  highlightedFactorValue: null | string;
+  isFocused: boolean;
+}>();
+
+const { error, score, vectorString } = useCvss4Calculations();
+const { cvss4Selections } = useCvss4Selections();
+
+function setMetric(category: string, metric: string, value: string) {
+  cvss4Selections.value[category][metric] = value;
+}
+</script>
+
+<template>
+  <Modal
+    :show="isFocused"
+    style="max-width: 75%;"
+    @mousedown="(event: MouseEvent) => event.preventDefault()"
+  >
+    <template #body>
+      <div
+        class="p-4 pt-2"
+      >
+        <div class="my-4 sticky-top p-2 bg-secondary text-white">
+          <b class="me-2">{{ score }}</b>{{ vectorString }}
+        </div>
+        <div
+          v-for="(category, categoryName) in CVSS4MetricsForUI"
+          :key="categoryName"
+          class="my-2 border bg-light-gray"
+        >
+          <div class="fw-bold px-3 py-2 bg-secondary text-white">{{ categoryName }}</div>
+          <div class="p-3 justify-content-between">
+            <div
+              v-for="(metricGroup, groupName) in category.metric_groups"
+              :key="groupName"
+              class="p-2"
+            >
+              <span
+                v-if="groupName"
+                class="fw-bold"
+                style="border-bottom: 1px solid; width: 35%; border-color: gray;"
+              >
+                {{ groupName }}
+              </span>
+              <div class="p-2">
+                <div
+                  v-for="(metric, metricName) in metricGroup as Record<string, any>"
+                  :key="metricName"
+                  class="p-2 btn-group-vertical btn-group-sm osim-factor-severity-select"
+                  style="vertical-align: top;"
+                >
+                  <button
+                    class="btn-group-header btn btn-secondary fw-bold lh-sm border"
+                    :class="{ 'osim-factor-highlight': metric.id === highlightedFactor}"
+                    style="max-height: 27.5px;"
+                    disabled
+                  >
+                    {{ metricName }}
+                  </button>
+                  <button
+                    v-for="{tooltip, value}, optionName in metric.options"
+                    :key="optionName"
+                    tabindex="-1"
+                    type="button"
+                    class="btn factor-btn lh-sm border"
+                    style="max-height: 27.5px;"
+                    :title="tooltip"
+                    :class="{
+                      selected: cvss4Selections?.[MetricNamesWithValues?.[categoryName]]?.[metric.short] === value
+                    }"
+                    @click="setMetric(MetricNamesWithValues[categoryName], metric.short, value)"
+                  >
+                    {{ optionName }}
+                  </button>
+                </div>
+              </div>
+              <div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </Modal>
+</template>
+
+<style scoped lang="scss">
+.cvss-calculator {
+  &.overlayed {
+    display: block;
+    transform: translateX(-25ch);
+    background-color: #525252;
+    border-radius: 10px;
+    z-index: 5;
+    position: absolute;
+  }
+
+  .osim-input {
+    display: flex;
+    width: 100%;
+
+    .row-group {
+      display: flex;
+      flex-direction: row;
+      margin-block: 10px;
+
+      .col-group {
+        display: flex;
+        flex-direction: column;
+        margin-inline: 5px;
+        width: 100%;
+
+        .btn-group-header {
+          background-color: #1f1f1f !important;
+          color: white;
+          border: 0;
+          padding-block: 7.5px;
+          font-weight: 600;
+
+          &.osim-factor-highlight {
+            background-color: hsl(200deg 100% 95%) !important;
+            color: hsl(200deg 100% 35%) !important;
+          }
+        }
+
+        .btn,
+        .btn:hover {
+          border: 1px;
+          margin: auto;
+        }
+
+        &:hover .osim-factor-highlight {
+          background-color: hsl(200deg 100% 95%) !important;
+          color: hsl(200deg 100% 35%) !important;
+        }
+      }
+    }
+  }
+}
+
+.factor-btn.selected {
+  background-color: gray;
+  color: white;
+}
+</style>
