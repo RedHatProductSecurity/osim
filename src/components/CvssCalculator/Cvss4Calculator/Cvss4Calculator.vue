3675517045182eb8a3a2ec3ddaed6caa4d857c8e
superbuggy <jpqreichard@gmail.com>
2025-05-13 09:42:25 -0400

    ✨ Support pasting for V4 and remove modal
    🚨 Fix linting issues
    
    💄 Clean up UI
    
    💄 Clean up UI

--- src/components/CvssCalculator/Cvss4Calculator/Cvss4Calculator.vue
+++ src/components/CvssCalculator/Cvss4Calculator/Cvss4Calculator.vue
@@ -4,9 +4,6 @@ import { watch } from 'vue';
 import { MetricNamesWithValues, CVSS4MetricsForUI }
   from '@/components/CvssCalculator/Cvss4Calculator/cvss4-ui-constants';
 
-// import { useCvss4Calculator } from '@/composables/useCvss4Calculator';
-
-import Modal from '@/widgets/Modal/Modal.vue';
 import type { ZodAffectType } from '@/types';
 
 const props = defineProps<{
@@ -14,7 +11,6 @@ const props = defineProps<{
   cvss4Score: null | number;
   cvss4Selections: any;
   cvss4Vector: null | string;
-  // cvss4Selections:  Record<string, Record<string, string>>;
   highlightedFactor: null | string;
   highlightedFactorValue: null | string;
   isFocused: boolean;
@@ -25,8 +21,6 @@ const emit = defineEmits<{
   'update:cvssVector': [value: null | string];
   'update:setMetric': [category: string, metric: string, value: string];
 }>();
-
-// const { cvss4Score, cvss4Selections, cvss4Vector, setMetric } = useCvssScores(props?.affect);
 
 function updateCvss4Score(newCvss4Score: null | number | undefined) {
   emit('update:cvssScore', newCvss4Score ?? null);
@@ -42,71 +36,63 @@ watch(() => props.cvss4Vector, updateCvss4Vector);
 </script>
 
 <template>
-  <Modal
-    :show="isFocused"
-    style="max-width: 75%;"
-    @mousedown="(event: MouseEvent) => event.preventDefault()"
-  >
-    <template #body>
+  <div class="p-4 pt-2 cvss-calculator">
+    <p class="mb-0 px-2 bg-light-gray">
+      <b class="me-2">{{ cvss4Score }}</b>
+      {{ cvss4Vector }}
+    </p>
+    <div
+      v-for="(category, categoryName) in CVSS4MetricsForUI"
+      :key="categoryName"
+      class="mb-2 bg-light-gray"
+    >
       <div
-        class="p-4 pt-2 cvss-calculator"
+        v-if="categoryName === 'Base Metrics'"
+        class="p-3 justify-content-between"
       >
-        <div class="my-4 sticky-top p-2 bg-secondary text-white">
-          <b class="me-2">{{ cvss4Score }}</b>{{ cvss4Vector }}
-        </div>
-        <details
-          v-for="(category, categoryName) in CVSS4MetricsForUI"
-          :key="categoryName"
-          :open="categoryName === 'Base Metrics'"
-          class="my-2 border bg-light-gray"
+        <div
+          v-for="(metricGroup, groupName) in category.metric_groups"
+          :key="groupName"
+          class="p-2"
         >
-          <summary class="fw-bold px-3 py-2 bg-secondary text-white">{{ categoryName }}</summary>
-          <div class="p-3 justify-content-between">
+          <div class="p-2">
             <div
-              v-for="(metricGroup, groupName) in category.metric_groups"
-              :key="groupName"
-              class="p-2"
+              v-for="(metric, metricName) in metricGroup as Record<string, any>"
+              :key="metricName"
+              class="p-2 btn-group-vertical btn-group-sm osim-factor-severity-select"
+              style="vertical-align: top;"
             >
-              <div class="p-2">
-                <div
-                  v-for="(metric, metricName) in metricGroup as Record<string, any>"
-                  :key="metricName"
-                  class="p-2 btn-group-vertical btn-group-sm osim-factor-severity-select"
-                  style="vertical-align: top;"
-                >
-                  <button
-                    class="btn-group-header btn btn-secondary fw-bold lh-sm border"
-                    :class="{ 'osim-factor-highlight': metric.id === highlightedFactor}"
-                    style="max-height: 27.5px;"
-                    disabled
-                  >
-                    {{ metricName }}
-                  </button>
-                  <button
-                    v-for="{tooltip, value}, optionName in metric.options"
-                    :key="optionName"
-                    tabindex="-1"
-                    type="button"
-                    class="btn factor-btn lh-sm border"
-                    style="max-height: 27.5px;"
-                    :title="tooltip"
-                    :class="{
-                      selected: cvss4Selections?.[MetricNamesWithValues?.[categoryName]]?.[metric.short] === value
-                    }"
-                    @click="emit('update:setMetric',MetricNamesWithValues[categoryName], metric.short, value)"
-                  >
-                    {{ optionName }}
-                  </button>
-                </div>
-              </div>
-              <div>
-              </div>
+              <button
+                class="btn-group-header btn btn-secondary fw-bold lh-sm border"
+                :class="{ 'osim-factor-highlight': metric.id === highlightedFactor}"
+                style="max-height: 27.5px;"
+                disabled
+              >
+                {{ metricName }}
+              </button>
+              <button
+                v-for="{tooltip, value}, optionName in metric.options"
+                :key="optionName"
+                tabindex="-1"
+                type="button"
+                class="btn factor-btn lh-sm border"
+                style="max-height: 27.5px;"
+                :title="tooltip"
+                :class="{
+                  selected: cvss4Selections?.[MetricNamesWithValues?.[categoryName]]?.[metric.short] === value
+                }"
+                @click="emit('update:setMetric',MetricNamesWithValues[categoryName], metric.short, value)"
+              >
+                {{ optionName }}
+              </button>
             </div>
           </div>
-        </details>
+          <div>
+          </div>
+        </div>
       </div>
-    </template>
-  </Modal>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
