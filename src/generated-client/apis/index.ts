077e6c57e081a0c1e9cdd67963283742385466d7
Alvaro Tinoco <atinocom@redhat.com>
2025-07-11 12:40:58 +0200

    generate apis

---
+++ src/generated-client/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './CollectorsApi';
+export * from './ExploitsApi';
+export * from './OsidbApi';
+export * from './TrackersApi';
+export * from './WorkflowsApi';
