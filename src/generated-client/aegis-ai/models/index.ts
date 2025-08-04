b10428a4cc5a0a623ac1b22dbd6618d9e233a6ee
C-Valen <charlie.valen@gmail.com>
2025-08-04 16:52:51 +0200

    👽️ Regenerate clients

---
+++ src/generated-client/aegis-ai/models/index.ts
@@ -0,0 +1,54 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * 
+ * @export
+ * @interface AegisAiWebSrcMainComponentFeatureName1
+ */
+export interface AegisAiWebSrcMainComponentFeatureName1 {
+}
+/**
+ * 
+ * @export
+ * @interface AegisAiWebSrcMainComponentFeatureName2
+ */
+export interface AegisAiWebSrcMainComponentFeatureName2 {
+}
+/**
+ * 
+ * @export
+ * @interface HTTPValidationError
+ */
+export interface HTTPValidationError {
+    /**
+     * 
+     * @type {any}
+     * @memberof HTTPValidationError
+     */
+    detail?: any | null;
+}
+/**
+ * 
+ * @export
+ * @interface ValidationError
+ */
+export interface ValidationError {
+    /**
+     * 
+     * @type {any}
+     * @memberof ValidationError
+     */
+    loc: any | null;
+    /**
+     * 
+     * @type {any}
+     * @memberof ValidationError
+     */
+    msg: any | null;
+    /**
+     * 
+     * @type {any}
+     * @memberof ValidationError
+     */
+    type: any | null;
+}
