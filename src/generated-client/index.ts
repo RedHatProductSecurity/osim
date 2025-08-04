1ed2e2c3c16674829ea5db8c94858f3cae985765
C-Valen <charlie.valen@gmail.com>
2025-08-04 16:52:51 +0200

    👽️ Regenerate clients

--- src/generated-client/index.ts
+++ src/generated-client/index.ts
@@ -1,3 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './models';
+
+// OSIDB API models/types
+export * from './osidb/models';
+
+// Aegis-AI API models/types
+export * from './aegis-ai/models';
