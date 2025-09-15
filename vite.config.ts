d75857c56904a80fa11851cfba8cb017dac1bbaf
Alvaro Tinoco <atinocom@redhat.com>
2025-09-15 16:30:20 +0200

    🔧 Add support for JSX

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import path from 'node:path';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import viteBasicSslPlugin from '@vitejs/plugin-basic-ssl';
 // import copy from '@rollup-extras/plugin-copy';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
     viteBasicSslPlugin(),
   ],
   build: {
@@ -54,4 +56,4 @@ export default defineConfig(() => ({
       // path: 'path-browserify',
     },
   },
-}));
+});
